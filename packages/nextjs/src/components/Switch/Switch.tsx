aeeea2920c78ea205edde7ff62683e8488aff4f2
Katherine Hennes <katherine@supergrain.com>
2023-02-03 14:30:36 -0800

    feat: show actual cron schedule in Switch component (#42)
    Found this helpful while working on incremental sync, so decided to
    check it in.
    
    - Adds a new boolean prop, `includeSyncDescription`, to the `Switch`
    component.
    - Also adds a new targetable `element` to the `appearance` prop
    (verified that overriding the `switchDescription` styles works 👍)
    - Moves responsibility for displaying the sync description from the
    client to the component lib
    - Adds a new dependency,
    [cronstrue](https://github.com/bradymholt/cRonstrue), to the `nextjs`
    package. Normally I'm leery of adding dependencies like this, but in
    this case, it's quick and easy to use, its bundle size is tiny, and it
    has no dependencies itself. Also, it has a cute name 💁‍♀️
    
    <img width="326" alt="Screen Shot 2023-02-03 at 2 09 35 PM"
    src="https://user-images.githubusercontent.com/2220186/216720965-4885c5d1-39eb-4c7d-a79e-a8fcef5ac6b4.png">
    
    Review [with whitespace
    removed](https://github.com/supaglue-labs/supaglue/pull/42/files?diff=unified&w=1)
    for an easier time.

--- packages/nextjs/src/components/Switch/Switch.tsx
+++ packages/nextjs/src/components/Switch/Switch.tsx
@@ -1,16 +1,19 @@
 /** @jsxImportSource @emotion/react */
 import * as RadixSwitch from '@radix-ui/react-switch';
 import classnames from 'classnames';
+import cronstrue from 'cronstrue';
 import { useId } from 'react';
 import useSWR from 'swr';
 import useSWRMutation from 'swr/mutation';
 import { updateSync } from '../../hooks/api';
+import { DeveloperConfig } from '../../lib/types';
 import { SupaglueProviderInternal } from '../../providers';
 import { useSupaglueContext } from '../../providers/SupaglueProvider';
 import { SupaglueAppearance } from '../../types';
 import { styles } from './styles';
 
 type Elements = {
+  switchDescription?: string;
   switchLabel?: string;
   switchOn?: string;
   switchOff?: string;
@@ -24,13 +27,16 @@ export type SwitchProps = {
   };
   className?: string;
   disabled?: boolean;
+  includeSyncDescription?: boolean;
   label?: string;
   syncConfigName: string;
 };
 
 const SwitchInternal = (props: SwitchProps) => {
-  const { appearance, disabled, label, syncConfigName } = props;
+  const { appearance, disabled, includeSyncDescription = true, label, syncConfigName } = props;
   const { customerId, apiUrl } = useSupaglueContext();
+
+  const { data: developerConfig } = useSWR<DeveloperConfig>({ path: '/developer_config' });
 
   // TODO: Use conditional fetching syntax
   const {
@@ -44,42 +50,57 @@ const SwitchInternal = (props: SwitchProps) => {
   const inputId = useId();
 
   const syncEnabled = !!sync?.enabled;
+  const syncConfig = developerConfig?.syncConfigs.find(({ name }) => name === syncConfigName);
 
   const { trigger: triggerUpdateSync } = useSWRMutation(`${apiUrl}/syncs/${sync?.id}`, updateSync);
 
   return (
-    <div
-      css={styles.switchWrapper}
-      className={classnames(props.className, 'sg-switchWrapper', appearance?.elements?.switchWrapper)}
-    >
-      <label className={classnames('sg-switchLabel', appearance?.elements?.switchLabel)} htmlFor={inputId}>
-        {label || `Sync ${syncConfigName}`}
-      </label>
-
-      <RadixSwitch.Root
-        id={inputId}
-        css={syncEnabled ? styles.switchRootOn : styles.switchRootOff}
-        checked={syncEnabled}
-        className={classnames(
-          'sg-switchRoot',
-          syncEnabled ? appearance?.elements?.switchOn : appearance?.elements?.switchOff
-        )}
-        disabled={disabled || isLoadingSync}
-        // Explicitly remove type attribute to prevent global css from resetting
-        // our styles
-        type={undefined}
-        onCheckedChange={async (checked: boolean) => {
-          const result = await triggerUpdateSync({ enabled: checked });
-          if (result?.data) {
-            mutate({ ...result.data });
-          }
-        }}
+    <div>
+      <div
+        css={styles.switchWrapper}
+        className={classnames(props.className, 'sg-switchWrapper', appearance?.elements?.switchWrapper)}
       >
-        <RadixSwitch.Thumb
-          css={styles.switchThumb}
-          className={classnames('sg-switchThumb', appearance?.elements?.switchThumb)}
-        />
-      </RadixSwitch.Root>
+        <label className={classnames('sg-switchLabel', appearance?.elements?.switchLabel)} htmlFor={inputId}>
+          {label || `Sync ${syncConfigName}`}
+        </label>
+
+        <RadixSwitch.Root
+          id={inputId}
+          css={syncEnabled ? styles.switchRootOn : styles.switchRootOff}
+          checked={syncEnabled}
+          className={classnames(
+            'sg-switchRoot',
+            syncEnabled ? appearance?.elements?.switchOn : appearance?.elements?.switchOff
+          )}
+          disabled={disabled || isLoadingSync}
+          // Explicitly remove type attribute to prevent global css from resetting
+          // our styles
+          type={undefined}
+          onCheckedChange={async (checked: boolean) => {
+            const result = await triggerUpdateSync({ enabled: checked });
+            if (result?.data) {
+              mutate({ ...result.data });
+            }
+          }}
+        >
+          <RadixSwitch.Thumb
+            css={styles.switchThumb}
+            className={classnames('sg-switchThumb', appearance?.elements?.switchThumb)}
+          />
+        </RadixSwitch.Root>
+      </div>
+
+      {includeSyncDescription && syncConfig && (
+        <p
+          className={classnames('sg-switchDescription', appearance?.elements?.switchDescription)}
+          css={styles.switchDescription}
+        >
+          {`${syncConfig.strategy === 'full_refresh' ? 'Fully refresh' : 'Sync'} all updated contacts ${cronstrue
+            .toString(syncConfig.cronExpression)
+            .toLowerCase()}`}
+          .
+        </p>
+      )}
     </div>
   );
 };
