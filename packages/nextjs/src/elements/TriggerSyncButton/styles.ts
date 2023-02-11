fd72f1e2ec2386e2170cc2f9cddb336f6a151362
Katherine Hennes <katherine@supaglue.com>
2023-02-10 17:16:23 -0800

    fix: vertically align toast with TriggerSyncButton

--- packages/nextjs/src/elements/TriggerSyncButton/styles.ts
+++ packages/nextjs/src/elements/TriggerSyncButton/styles.ts
@@ -12,14 +12,17 @@ const toast = _applyTheme((theme: SgTheme) =>
     padding: '0.25rem 0.5rem',
     display: 'block',
     left: '9rem',
-    top: '0.375rem',
+    top: 0,
     borderRadius: '0.25rem',
     fontSize: '0.75rem',
     lineHeight: '1rem',
+    width: '100%',
     ...theme.elementOverrides?.toast,
   })
 );
 
-export default {
+const styles = {
   toast,
 };
+
+export default styles;
