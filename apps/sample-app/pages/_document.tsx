2d12f113675032cfec4c32255d5960d434ffeefd
Ryan Liu <ryan@supergrain.com>
2023-02-14 17:16:41 -0800

    fix(sample-app): Add back default light theme for consistency

--- apps/sample-app/pages/_document.tsx
+++ apps/sample-app/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en" className="h-full bg-gray-100">
+    <Html data-theme="light" lang="en" className="h-full bg-gray-100">
       <Head />
       <body className="h-full">
         <Main />
