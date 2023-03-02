1f509861bd81a7ab1228aab6116eac18d10cc7b5
Ryan Liu <ryan@supaglue.com>
2023-03-02 12:56:23 -0800

    fix: dep container

--- apps/api/dependency_container.ts
+++ apps/api/dependency_container.ts
@@ -14,7 +14,7 @@ let dependencyContainer: DependencyContainer | undefined = undefined;
 
 function createDependencyContainer(): DependencyContainer {
   const coreDependencyContainer = getCoreDependencyContainer();
-  const { prisma } = coreDependencyContainer;
+  const { prisma, integrationService } = coreDependencyContainer;
 
   const temporalClient = new Client({
     connection: Connection.lazy({
@@ -23,7 +23,7 @@ function createDependencyContainer(): DependencyContainer {
   });
 
   const syncService = new SyncService(temporalClient);
-  const connectionWriterService = new ConnectionWriterService(prisma, syncService);
+  const connectionWriterService = new ConnectionWriterService(prisma, syncService, integrationService);
 
   return {
     ...coreDependencyContainer,
