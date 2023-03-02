b57a334413e67c462670cd5f065753ccca264855
Lucas Marshall <lucasmarshall@users.noreply.github.com>
2023-03-02 14:38:15 -0800

    chore: create sync history table (#191)

---
+++ packages/db/prisma/migrations/20230302223137_create_sync_history_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "sync_history" (
+    "id" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
+    "result" TEXT NOT NULL,
+    "error_message" TEXT,
+    "start_timestamp" TIMESTAMP(3) NOT NULL,
+    "end_timestamp" TIMESTAMP(3),
+    "connection_id" TEXT NOT NULL,
+
+    CONSTRAINT "sync_history_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "sync_history" ADD CONSTRAINT "sync_history_connection_id_fkey" FOREIGN KEY ("connection_id") REFERENCES "connections"("id") ON DELETE CASCADE ON UPDATE CASCADE;
