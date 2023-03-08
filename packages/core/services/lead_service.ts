ceaf9de9f95f660e3393165d7e1731bba577be21
Albert <albert@supaglue.com>
2023-03-07 16:16:52 -0800

    feat: really janky version of COPY to postgres for syncs (#212)
    there is still a lot more to do here. for example:
    - we manually list out columns right now. maybe we can get them from
    prisma somehow, or we should ditch prisma for common model
    - we should stream data into COPY from SF instead of doing things
    serially (pipelining)
    - on initial load, we can avoid doing the temp table COPY and just write
    directly to the common model table
    - don't manually write `api` schema. rely on config instead

--- packages/core/services/lead_service.ts
+++ packages/core/services/lead_service.ts
@@ -1,4 +1,6 @@
-import type { PrismaClient } from '@supaglue/db';
+import { stringify } from 'csv-stringify';
+import { from as copyFrom } from 'pg-copy-streams';
+import { v4 as uuidv4 } from 'uuid';
 import { NotFoundError, UnauthorizedError } from '../errors';
 import { POSTGRES_UPDATE_BATCH_SIZE } from '../lib/constants';
 import { getExpandedAssociations } from '../lib/expand';
@@ -13,22 +15,18 @@ import type {
   ListParams,
   PaginatedResult,
 } from '../types';
-import type { RemoteService } from './remote_service';
+import { CommonModelBaseService } from './common_model_base_service';
 
-export class LeadService {
-  #prisma: PrismaClient;
-  #remoteService: RemoteService;
-
-  constructor(prisma: PrismaClient, remoteService: RemoteService) {
-    this.#prisma = prisma;
-    this.#remoteService = remoteService;
+export class LeadService extends CommonModelBaseService {
+  public constructor(...args: ConstructorParameters<typeof CommonModelBaseService>) {
+    super(...args);
   }
 
   // TODO: implement getParams
   public async getById(id: string, connectionId: string, getParams: GetParams): Promise<Lead> {
     const { expand } = getParams;
     const expandedAssociations = getExpandedAssociations(expand);
-    const model = await this.#prisma.crmLead.findUnique({
+    const model = await this.prisma.crmLead.findUnique({
       where: { id },
       include: {
         convertedAccount: expandedAssociations.includes('converted_account'),
@@ -49,7 +47,7 @@ export class LeadService {
     const { page_size, cursor, created_after, created_before, updated_after, updated_before, expand } = listParams;
     const expandedAssociations = getExpandedAssociations(expand);
     const pageSize = page_size ? parseInt(page_size) : undefined;
-    const models = await this.#prisma.crmLead.findMany({
+    const models = await this.prisma.crmLead.findMany({
       ...getPaginationParams(pageSize, cursor),
       where: {
         connectionId,
@@ -80,9 +78,9 @@ export class LeadService {
   public async create(customerId: string, connectionId: string, createParams: LeadCreateParams): Promise<Lead> {
     // TODO: We may want to have better guarantees that we update the record in both our DB
     // and the external integration.
-    const remoteClient = await this.#remoteService.getCrmRemoteClient(connectionId);
+    const remoteClient = await this.remoteService.getCrmRemoteClient(connectionId);
     const remoteLead = await remoteClient.createLead(createParams);
-    const leadModel = await this.#prisma.crmLead.create({
+    const leadModel = await this.prisma.crmLead.create({
       data: {
         customerId,
         connectionId,
@@ -95,7 +93,7 @@ export class LeadService {
   public async update(customerId: string, connectionId: string, updateParams: LeadUpdateParams): Promise<Lead> {
     // TODO: We may want to have better guarantees that we update the record in both our DB
     // and the external integration.
-    const foundLeadModel = await this.#prisma.crmLead.findUniqueOrThrow({
+    const foundLeadModel = await this.prisma.crmLead.findUniqueOrThrow({
       where: {
         id: updateParams.id,
       },
@@ -105,13 +103,13 @@ export class LeadService {
       throw new Error('Lead customerId does not match');
     }
 
-    const remoteClient = await this.#remoteService.getCrmRemoteClient(connectionId);
+    const remoteClient = await this.remoteService.getCrmRemoteClient(connectionId);
     const remoteLead = await remoteClient.updateLead({
       ...updateParams,
       remoteId: foundLeadModel.remoteId,
     });
 
-    const leadModel = await this.#prisma.crmLead.update({
+    const leadModel = await this.prisma.crmLead.update({
       data: remoteLead,
       where: {
         id: updateParams.id,
@@ -120,20 +118,100 @@ export class LeadService {
     return fromLeadModel(leadModel);
   }
 
-  // TODO: We should do this performantly, which may involve COPY to temp table
-  // and bulk upsert from there.
   public async upsertRemoteLeads(connectionId: string, upsertParamsList: LeadSyncUpsertParams[]): Promise<void> {
-    for (const upsertParams of upsertParamsList) {
-      await this.#prisma.crmLead.upsert({
-        where: {
-          connectionId_remoteId: {
-            connectionId,
-            remoteId: upsertParams.remoteId,
-          },
-        },
-        update: upsertParams,
-        create: upsertParams,
+    const client = await this.pgPool.connect();
+
+    // TODO: On the first run, we should be able to directly write into the table and skip the temp table
+
+    try {
+      // TODO: Get schema (api) from config
+      const table = 'api.crm_leads';
+      const tempTable = 'crm_leads_temp';
+
+      // Create a temporary table
+      // TODO: In the future, we may want to create a permanent table with background reaper
+      // so that we can resume in the case of failure during the COPY stage.
+      // TODO: Maybe we don't need to include all
+      await client.query(`CREATE TEMP TABLE IF NOT EXISTS ${tempTable} (LIKE ${table} INCLUDING ALL)`);
+
+      // TODO: Define columns and mappers elsewhere
+      // Columns
+      const columnsWithoutId = [
+        'remote_id',
+        'customer_id',
+        'connection_id',
+        'remote_was_deleted',
+        'owner',
+        'lead_source',
+        'title',
+        'company',
+        'first_name',
+        'last_name',
+        'addresses',
+        'phone_numbers',
+        'email_addresses',
+        'remote_created_at',
+        'remote_updated_at',
+        'converted_date',
+        '_converted_remote_account_id',
+        '_converted_remote_contact_id',
+        'updated_at', // TODO: We should have default for this column in Postgres
+      ];
+      const columns = ['id', ...columnsWithoutId];
+
+      // Output
+      const stream = client.query(
+        copyFrom(`COPY ${tempTable} (${columns.join(',')}) FROM STDIN WITH (DELIMITER ',', FORMAT CSV)`)
+      );
+
+      // Input
+      const convertedUpsertParamsList = upsertParamsList.map((upsertParams) => ({
+        id: uuidv4(),
+        remote_id: upsertParams.remoteId,
+        customer_id: upsertParams.customerId,
+        connection_id: upsertParams.connectionId,
+        remote_was_deleted: upsertParams.remoteWasDeleted,
+        owner: upsertParams.owner,
+        lead_source: upsertParams.leadSource,
+        title: upsertParams.title,
+        company: upsertParams.company,
+        first_name: upsertParams.firstName,
+        last_name: upsertParams.lastName,
+        addresses: upsertParams.addresses,
+        email_addresses: upsertParams.emailAddresses,
+        remote_created_at: upsertParams.remoteCreatedAt?.toISOString(),
+        remote_updated_at: upsertParams.remoteUpdatedAt?.toISOString(),
+        converted_date: upsertParams.convertedDate?.toISOString(),
+        _converted_remote_account_id: upsertParams.convertedRemoteAccountId,
+        _converted_remote_contact_id: upsertParams.convertedRemoteContactId,
+        updated_at: new Date().toISOString(),
+      }));
+
+      await new Promise((resolve, reject) => {
+        const csvInput = stringify(
+          convertedUpsertParamsList.map((upsertParams) => ({ ...upsertParams, id: uuidv4() })),
+          {
+            columns,
+            cast: {
+              boolean: (value: boolean) => value.toString(),
+            },
+          }
+        );
+        csvInput.on('error', reject);
+        stream.on('error', reject);
+        stream.on('finish', resolve);
+        csvInput.pipe(stream);
       });
+
+      // Copy from temp table
+      const columnsToUpdate = columnsWithoutId.join(',');
+      const excludedDolumnsToUpdate = columnsWithoutId.map((column) => `EXCLUDED.${column}`).join(',');
+      await client.query(`INSERT INTO ${table}
+SELECT * FROM ${tempTable}
+ON CONFLICT (connection_id, remote_id)
+DO UPDATE SET (${columnsToUpdate}) = (${excludedDolumnsToUpdate})`);
+    } finally {
+      client.release();
     }
   }
 
@@ -142,7 +220,7 @@ export class LeadService {
     limit: number,
     cursor?: string
   ): Promise<string | undefined> {
-    const leadsWithDanglingAccounts = await this.#prisma.crmLead.findMany({
+    const leadsWithDanglingAccounts = await this.prisma.crmLead.findMany({
       where: {
         connectionId,
         convertedAccountId: null,
@@ -164,7 +242,7 @@ export class LeadService {
     const danglingRemoteAccountIds = leadsWithDanglingAccounts.map(
       ({ convertedRemoteAccountId }) => convertedRemoteAccountId
     ) as string[];
-    const crmAccounts = await this.#prisma.crmAccount.findMany({
+    const crmAccounts = await this.prisma.crmAccount.findMany({
       where: {
         connectionId,
         remoteId: {
@@ -174,7 +252,7 @@ export class LeadService {
     });
     await Promise.all(
       crmAccounts.map(({ remoteId, id }) => {
-        return this.#prisma.crmLead.updateMany({
+        return this.prisma.crmLead.updateMany({
           where: {
             connectionId,
             convertedRemoteAccountId: remoteId,
@@ -200,7 +278,7 @@ export class LeadService {
     limit: number,
     cursor?: string
   ): Promise<string | undefined> {
-    const leadsWithDanglingContacts = await this.#prisma.crmLead.findMany({
+    const leadsWithDanglingContacts = await this.prisma.crmLead.findMany({
       where: {
         connectionId,
         convertedContactId: null,
@@ -222,7 +300,7 @@ export class LeadService {
     const danglingRemoteContactIds = leadsWithDanglingContacts.map(
       ({ convertedRemoteContactId }) => convertedRemoteContactId
     ) as string[];
-    const crmContacts = await this.#prisma.crmContact.findMany({
+    const crmContacts = await this.prisma.crmContact.findMany({
       where: {
         connectionId,
         remoteId: {
@@ -232,7 +310,7 @@ export class LeadService {
     });
     await Promise.all(
       crmContacts.map(({ remoteId, id }) => {
-        return this.#prisma.crmLead.updateMany({
+        return this.prisma.crmLead.updateMany({
           where: {
             connectionId,
             convertedRemoteContactId: remoteId,
--- packages/core/services/opportunity_service.ts
+++ packages/core/services/opportunity_service.ts
@@ -1,4 +1,6 @@
-import type { PrismaClient } from '@supaglue/db';
+import { stringify } from 'csv-stringify';
+import { from as copyFrom } from 'pg-copy-streams';
+import { v4 as uuidv4 } from 'uuid';
 import { NotFoundError, UnauthorizedError } from '../errors';
 import { POSTGRES_UPDATE_BATCH_SIZE } from '../lib/constants';
 import { getExpandedAssociations } from '../lib/expand';
@@ -13,22 +15,18 @@ import type {
   OpportunityUpdateParams,
   PaginatedResult,
 } from '../types';
-import type { RemoteService } from './remote_service';
+import { CommonModelBaseService } from './common_model_base_service';
 
-export class OpportunityService {
-  #prisma: PrismaClient;
-  #remoteService: RemoteService;
-
-  constructor(prisma: PrismaClient, remoteService: RemoteService) {
-    this.#prisma = prisma;
-    this.#remoteService = remoteService;
+export class OpportunityService extends CommonModelBaseService {
+  public constructor(...args: ConstructorParameters<typeof CommonModelBaseService>) {
+    super(...args);
   }
 
   // TODO: implement getParams
   public async getById(id: string, connectionId: string, getParams: GetParams): Promise<Opportunity> {
     const { expand } = getParams;
     const expandedAssociations = getExpandedAssociations(expand);
-    const model = await this.#prisma.crmOpportunity.findUnique({
+    const model = await this.prisma.crmOpportunity.findUnique({
       where: { id },
       include: {
         account: expandedAssociations.includes('account'),
@@ -48,7 +46,7 @@ export class OpportunityService {
     const { page_size, cursor, created_after, created_before, updated_after, updated_before, expand } = listParams;
     const expandedAssociations = getExpandedAssociations(expand);
     const pageSize = page_size ? parseInt(page_size) : undefined;
-    const models = await this.#prisma.crmOpportunity.findMany({
+    const models = await this.prisma.crmOpportunity.findMany({
       ...getPaginationParams(pageSize, cursor),
       where: {
         connectionId,
@@ -76,7 +74,7 @@ export class OpportunityService {
   }
 
   private async getAssociatedAccountRemoteId(accountId: string): Promise<string> {
-    const crmAccount = await this.#prisma.crmAccount.findUnique({
+    const crmAccount = await this.prisma.crmAccount.findUnique({
       where: {
         id: accountId,
       },
@@ -98,9 +96,9 @@ export class OpportunityService {
     if (createParams.accountId) {
       remoteCreateParams.accountId = await this.getAssociatedAccountRemoteId(createParams.accountId);
     }
-    const remoteClient = await this.#remoteService.getCrmRemoteClient(connectionId);
+    const remoteClient = await this.remoteService.getCrmRemoteClient(connectionId);
     const remoteOpportunity = await remoteClient.createOpportunity(remoteCreateParams);
-    const opportunityModel = await this.#prisma.crmOpportunity.create({
+    const opportunityModel = await this.prisma.crmOpportunity.create({
       data: {
         customerId,
         connectionId,
@@ -118,7 +116,7 @@ export class OpportunityService {
   ): Promise<Opportunity> {
     // TODO: We may want to have better guarantees that we update the record in both our DB
     // and the external integration.
-    const foundOpportunityModel = await this.#prisma.crmOpportunity.findUniqueOrThrow({
+    const foundOpportunityModel = await this.prisma.crmOpportunity.findUniqueOrThrow({
       where: {
         id: updateParams.id,
       },
@@ -133,13 +131,13 @@ export class OpportunityService {
       remoteUpdateParams.accountId = await this.getAssociatedAccountRemoteId(updateParams.accountId);
     }
 
-    const remoteClient = await this.#remoteService.getCrmRemoteClient(connectionId);
+    const remoteClient = await this.remoteService.getCrmRemoteClient(connectionId);
     const remoteOpportunity = await remoteClient.updateOpportunity({
       ...remoteUpdateParams,
       remoteId: foundOpportunityModel.remoteId,
     });
 
-    const opportunityModel = await this.#prisma.crmOpportunity.update({
+    const opportunityModel = await this.prisma.crmOpportunity.update({
       data: { ...remoteOpportunity, accountId: updateParams.accountId },
       where: {
         id: updateParams.id,
@@ -148,23 +146,98 @@ export class OpportunityService {
     return fromOpportunityModel(opportunityModel);
   }
 
-  // TODO: We should do this performantly, which may involve COPY to temp table
-  // and bulk upsert from there.
   public async upsertRemoteOpportunities(
     connectionId: string,
     upsertParamsList: OpportunitySyncUpsertParams[]
   ): Promise<void> {
-    for (const upsertParams of upsertParamsList) {
-      await this.#prisma.crmOpportunity.upsert({
-        where: {
-          connectionId_remoteId: {
-            connectionId,
-            remoteId: upsertParams.remoteId,
-          },
-        },
-        update: upsertParams,
-        create: upsertParams,
+    const client = await this.pgPool.connect();
+
+    // TODO: On the first run, we should be able to directly write into the table and skip the temp table
+
+    try {
+      // TODO: Get schema (api) from config
+      const table = 'api.crm_opportunities';
+      const tempTable = 'crm_opportunities_temp';
+
+      // Create a temporary table
+      // TODO: In the future, we may want to create a permanent table with background reaper
+      // so that we can resume in the case of failure during the COPY stage.
+      // TODO: Maybe we don't need to include all
+      await client.query(`CREATE TEMP TABLE IF NOT EXISTS ${tempTable} (LIKE ${table} INCLUDING ALL)`);
+
+      // TODO: Define columns and mappers elsewhere
+      // Columns
+      const columnsWithoutId = [
+        'remote_id',
+        'customer_id',
+        'connection_id',
+        'remote_was_deleted',
+        'owner',
+        'name',
+        'description',
+        'amount',
+        'stage',
+        'status',
+        'last_activity_at',
+        'close_date',
+        'remote_created_at',
+        'remote_updated_at',
+        '_remote_account_id',
+        'updated_at', // TODO: We should have default for this column in Postgres
+      ];
+      const columns = ['id', ...columnsWithoutId];
+
+      // Output
+      const stream = client.query(
+        copyFrom(`COPY ${tempTable} (${columns.join(',')}) FROM STDIN WITH (DELIMITER ',', FORMAT CSV)`)
+      );
+
+      // Input
+      const convertedUpsertParamsList = upsertParamsList.map((upsertParams) => ({
+        id: uuidv4(),
+        remote_id: upsertParams.remoteId,
+        customer_id: upsertParams.customerId,
+        connection_id: upsertParams.connectionId,
+        remote_was_deleted: upsertParams.remoteWasDeleted,
+        owner: upsertParams.owner,
+        name: upsertParams.name,
+        description: upsertParams.description,
+        amount: upsertParams.amount,
+        stage: upsertParams.stage,
+        status: upsertParams.status,
+        last_activity_at: upsertParams.lastActivityAt?.toISOString(),
+        close_date: upsertParams.closeDate?.toISOString(),
+        remote_created_at: upsertParams.remoteCreatedAt?.toISOString(),
+        remote_updated_at: upsertParams.remoteUpdatedAt?.toISOString(),
+        _remote_account_id: upsertParams.remoteAccountId,
+        updated_at: new Date().toISOString(),
+      }));
+
+      await new Promise((resolve, reject) => {
+        const csvInput = stringify(
+          convertedUpsertParamsList.map((upsertParams) => ({ ...upsertParams, id: uuidv4() })),
+          {
+            columns,
+            cast: {
+              boolean: (value: boolean) => value.toString(),
+            },
+          }
+        );
+        csvInput.on('error', reject);
+        stream.on('error', reject);
+        stream.on('finish', resolve);
+        csvInput.pipe(stream);
       });
+
+      // Copy from temp table
+      const columnsToUpdate = columnsWithoutId.join(',');
+      const excludedDolumnsToUpdate = columnsWithoutId.map((column) => `EXCLUDED.${column}`).join(',');
+      await client.query(`INSERT INTO ${table}
+SELECT * FROM ${tempTable}
+ON CONFLICT (connection_id, remote_id)
+DO UPDATE SET (${columnsToUpdate}) = (${excludedDolumnsToUpdate})`);
+    } finally {
+      client.release();
     }
   }
 
@@ -173,7 +246,7 @@ export class OpportunityService {
     limit: number,
     cursor?: string
   ): Promise<string | undefined> {
-    const opportunitiesWithDanglingAccounts = await this.#prisma.crmOpportunity.findMany({
+    const opportunitiesWithDanglingAccounts = await this.prisma.crmOpportunity.findMany({
       where: {
         connectionId,
         accountId: null,
@@ -195,7 +268,7 @@ export class OpportunityService {
     const danglingRemoteAccountIds = opportunitiesWithDanglingAccounts.map(
       ({ remoteAccountId }) => remoteAccountId
     ) as string[];
-    const crmAccounts = await this.#prisma.crmAccount.findMany({
+    const crmAccounts = await this.prisma.crmAccount.findMany({
       where: {
         connectionId,
         remoteId: {
@@ -205,7 +278,7 @@ export class OpportunityService {
     });
     await Promise.all(
       crmAccounts.map(({ remoteId, id }) => {
-        return this.#prisma.crmOpportunity.updateMany({
+        return this.prisma.crmOpportunity.updateMany({
           where: {
             connectionId,
             remoteAccountId: remoteId,
