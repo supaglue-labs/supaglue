8be06e91fae4454def0d14f4f76b9ed25ecf2ad7
Albert Wu <albert@supaglue.com>
2023-03-10 08:39:57 -0800

    fix: broken openapi specs/validation middleware

--- apps/api/routes/mgmt/v1/index.ts
+++ apps/api/routes/mgmt/v1/index.ts
@@ -1,9 +1,12 @@
+import { openapiMiddleware } from '@/middleware/openapi';
 import { Router } from 'express';
 import customer from './customer';
 import integration from './integration';
 
 export default function init(app: Router): void {
   const v1Router = Router();
+
+  v1Router.use(openapiMiddleware('mgmt'));
 
   customer(v1Router);
   integration(v1Router);
