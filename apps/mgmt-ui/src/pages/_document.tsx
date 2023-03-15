58f471f485aae01684550422a967fa0c8fe4b868
george-xing <1925713+george-xing@users.noreply.github.com>
2023-03-14 23:39:22 -0700

    fix: import roboto fonts and clean up sidebar in management portal (#276)
    Co-authored-by: George Xing <george@supaglue.com>

--- apps/mgmt-ui/src/pages/_document.tsx
+++ apps/mgmt-ui/src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
