8c477146201496ed6d49c3af1a249e45a86723a6
thomas chen <tomkit@gmail.com>
2023-03-16 13:09:40 -0700

    chore: developer opted-in deployment id (#295)
    - use `SUPAGLUE_DEPLOYMENT_ID` if a developer opts in, suffix it to
    avoid collisions
    
    SUP1-62

--- packages/core/lib/distinct_identifier.ts
+++ packages/core/lib/distinct_identifier.ts
@@ -1,25 +1,33 @@
+import crypto from 'crypto';
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
+const sgDeploymentId = process.env.SUPAGLUE_DEPLOYMENT_ID;
 const configPath = path.join(os.homedir(), '.supaglue', 'session.json');
 let distinctIdentifier: string | undefined = undefined;
+
+function generateDistinctId() {
+  const suffix = crypto.randomBytes(4).toString('hex');
+  return sgDeploymentId ? `${sgDeploymentId}-${suffix}` : uuidv4();
+}
 
 // read distinctId from config file or write it if it doesn't exist
 if (fs.existsSync(configPath)) {
   const session = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
   if (!session.distinctIdentifier) {
-    session.distinctIdentifier = uuidv4();
+    session.distinctIdentifier = generateDistinctId();
     fs.writeFileSync(configPath, JSON.stringify(session), 'utf8');
   }
 
   ({ distinctIdentifier } = session);
 } else {
-  distinctIdentifier = uuidv4();
+  distinctIdentifier = generateDistinctId();
   fs.mkdirSync(path.join(os.homedir(), '.supaglue'), { recursive: true });
   fs.writeFileSync(configPath, JSON.stringify({ distinctIdentifier }));
 }
 
+// The distinctIdentifier "session" lives from the time a developer runs docker compose to the time that they delete `session.json` from their home directory on the filesystem. Locally, it persists across docker compose up/down/restart and updates.
 export const distinctId = distinctIdentifier && `session:${distinctIdentifier}`;
