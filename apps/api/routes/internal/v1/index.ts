324684abf4b14748550e9f582c1d036530cf0312
Ryan Liu <ryan@supaglue.com>
2023-03-17 16:08:57 -0700

    feat: add internal endpoints too

--- apps/api/routes/internal/v1/index.ts
+++ apps/api/routes/internal/v1/index.ts
@@ -4,6 +4,8 @@ import apiKey from './api_key';
 import auth from './auth';
 import customer from './customer';
 import integration from './integration';
+import syncHistory from './sync_history';
+import syncInfo from './sync_info';
 import webhook from './webhook';
 
 export default function init(app: Router): void {
@@ -15,6 +17,8 @@ export default function init(app: Router): void {
   customer(v1Router);
   integration(v1Router);
   webhook(v1Router);
+  syncInfo(v1Router);
+  syncHistory(v1Router);
 
   app.use('/v1', v1Router);
 }
---
+++ apps/api/routes/internal/v1/sync_history.ts
@@ -0,0 +1,38 @@
+import { getDependencyContainer } from '@/dependency_container';
+import { snakecaseKeys } from '@/lib/snakecase';
+import {
+  GetSyncHistoryPathParams,
+  GetSyncHistoryQueryParams,
+  GetSyncHistoryRequest,
+  GetSyncHistoryResponse,
+} from '@supaglue/schemas/mgmt';
+import { Request, Response, Router } from 'express';
+
+const { syncHistoryService } = getDependencyContainer();
+
+export default function init(app: Router) {
+  app.get(
+    '/sync-history',
+    async (
+      req: Request<GetSyncHistoryPathParams, GetSyncHistoryResponse, GetSyncHistoryRequest, GetSyncHistoryQueryParams>,
+      res: Response<GetSyncHistoryResponse>
+    ) => {
+      const { next, previous, results } = await syncHistoryService.list({
+        applicationId: req.supaglueApplication.id,
+        paginationParams: { page_size: req.query?.page_size, cursor: req.query?.cursor },
+        model: req.query?.model,
+        customerId: req.query?.customer_id,
+        providerName: req.query?.provider_name,
+      });
+
+      const snakeCaseResults = results.map((result) =>
+        snakecaseKeys({
+          ...result,
+          startTimestamp: result.startTimestamp.toISOString(),
+          endTimestamp: result.endTimestamp?.toISOString(),
+        })
+      );
+      return res.status(200).send({ next, previous, results: snakeCaseResults });
+    }
+  );
+}
