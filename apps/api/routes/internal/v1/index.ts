138bb5236a11315b20973b6f33c6e50e4c8f8839
Albert Wu <albert@supaglue.com>
2023-03-20 14:28:36 -0700

    fix: middleware for application routes

--- apps/api/routes/internal/v1/index.ts
+++ apps/api/routes/internal/v1/index.ts
@@ -1,3 +1,4 @@
+import { applicationMiddleware } from '@/middleware/application';
 import { internalMiddleware } from '@/middleware/internal';
 import { Router } from 'express';
 import apiKey from './api_key';
@@ -10,12 +11,21 @@ import syncInfo from './sync_info';
 import webhook from './webhook';
 
 export default function init(app: Router): void {
+  // application routes should not require application header
+  const v1ApplicationRouter = Router();
+  v1ApplicationRouter.use(internalMiddleware);
+
+  application(v1ApplicationRouter);
+
+  app.use('/v1', v1ApplicationRouter);
+
+  // non-application routes require application header
   const v1Router = Router();
   v1Router.use(internalMiddleware);
+  v1Router.use(applicationMiddleware);
 
   auth(v1Router);
   apiKey(v1Router);
-  application(v1Router);
   customer(v1Router);
   integration(v1Router);
   webhook(v1Router);
