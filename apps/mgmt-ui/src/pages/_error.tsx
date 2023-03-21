b0350974ce7f892f42ee67a7a25343801734f955
Lucas Marshall <lucas@supergrain.com>
2023-03-21 15:07:48 -0700

    fix: typescript

--- apps/mgmt-ui/src/pages/_error.tsx
+++ apps/mgmt-ui/src/pages/_error.tsx
@@ -19,7 +19,7 @@
 import * as Sentry from '@sentry/nextjs';
 import NextErrorComponent from 'next/error';
 
-const CustomErrorComponent = (props) => {
+const CustomErrorComponent = (props: any) => {
   // If you're using a Nextjs version prior to 12.2.1, uncomment this to
   // compensate for https://github.com/vercel/next.js/issues/8592
   // Sentry.captureUnderscoreErrorException(props);
@@ -27,7 +27,7 @@ const CustomErrorComponent = (props) => {
   return <NextErrorComponent statusCode={props.statusCode} />;
 };
 
-CustomErrorComponent.getInitialProps = async (contextData) => {
+CustomErrorComponent.getInitialProps = async (contextData: any) => {
   // In case this is running in a serverless function, await this in order to give Sentry
   // time to send the error before the lambda exits
   await Sentry.captureUnderscoreErrorException(contextData);
