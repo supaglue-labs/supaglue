cdcf1b7fdd1bae5bfb6a5b2abbd9ea1cb3f0f014
Lucas Marshall <lucasmarshall@users.noreply.github.com>
2023-03-21 15:20:45 -0700

    chore: add sentry error logging to ui (#357)

--- apps/mgmt-ui/next.config.js
+++ apps/mgmt-ui/next.config.js
@@ -1,3 +1,10 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withSentryConfig } = require('@sentry/nextjs');
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
@@ -21,3 +28,5 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+module.exports = withSentryConfig(module.exports, { silent: true }, { hideSourcemaps: false });
