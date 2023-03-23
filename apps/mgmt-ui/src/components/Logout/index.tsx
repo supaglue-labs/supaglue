1d8e5e396e4eb23971068a54fecf07a56e3fbf1e
Ryan Liu <ryan@supaglue.com>
2023-03-23 10:45:38 -0700

    chore: new logout component

---
+++ apps/mgmt-ui/src/components/Logout/index.tsx
@@ -0,0 +1,38 @@
+import { IS_CLOUD } from '@/pages/api';
+import { useClerk } from '@clerk/nextjs';
+import MUILogout from '@mui/icons-material/Logout';
+import { ListItemIcon, MenuItem } from '@mui/material';
+import { signOut } from 'next-auth/react';
+
+const LogoutImpl = ({ handleLogout }: { handleLogout: () => void }) => {
+  return (
+    <MenuItem onClick={handleLogout}>
+      <ListItemIcon>
+        <MUILogout fontSize="small" />
+      </ListItemIcon>
+      Logout
+    </MenuItem>
+  );
+};
+
+const NonCloudLogout = () => {
+  const handleLogout = async () => {
+    await signOut();
+  };
+  return <LogoutImpl handleLogout={handleLogout} />;
+};
+
+const CloudLogout = () => {
+  const { signOut: clerkSignOut } = useClerk();
+  const handleLogout = async () => {
+    await clerkSignOut();
+  };
+  return <LogoutImpl handleLogout={handleLogout} />;
+};
+
+export const Logout = () => {
+  if (IS_CLOUD) {
+    return <CloudLogout />;
+  }
+  return <NonCloudLogout />;
+};
