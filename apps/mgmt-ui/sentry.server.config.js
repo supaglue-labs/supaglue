9b325b79898555ce07d54b0b7b457c41395f1788
Lucas Marshall <lucas@supergrain.com>
2023-03-28 10:54:46 -0700

    fix: don't do sentry traces

--- apps/mgmt-ui/sentry.server.config.js
+++ apps/mgmt-ui/sentry.server.config.js
@@ -13,8 +13,6 @@ const SENTRY_ENABLED = !(
 
 Sentry.init({
   dsn: SENTRY_DSN || 'https://c9b7660599b7410e972b290a1e62dd5f@o4504573112745984.ingest.sentry.io/4504878650753024',
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
