5370f62acfa28d01133b396dcb5fa579f95abe44
Albert Wu <albert@supaglue.com>
2023-04-03 12:53:19 -0700

    chore: improve mgmt ui performance locally

--- apps/mgmt-ui/next.config.js
+++ apps/mgmt-ui/next.config.js
@@ -17,6 +17,14 @@ const nextConfig = {
     outputFileTracingRoot: path.join(__dirname, '../../'),
   },
   transpilePackages: ['@supaglue/types', '@supaglue/utils'],
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
 };
 
 module.exports = nextConfig;
