0f13358e72f1ef6a0c715ec6891b8538530880d4
Albert <albert@supaglue.com>
2023-04-03 17:09:28 -0700

    feat: allow update name and delete for application (#527)
    <!-- Please title your PR using conventional commits
    (https://www.conventionalcommits.org/en/v1.0.0/): -->
    
    Fixes: #500 
    
    <img width="1313" alt="image"
    src="https://user-images.githubusercontent.com/1498227/229255087-e90655a7-8f05-4c28-8aea-73e24939cd5e.png">
    
    ## Test Plan
    
    [Describe test plan here]
    
    ## Deployment instructions
    
    [Add any special deployment instructions here]

--- apps/mgmt-ui/src/components/ApplicationMenu/index.tsx
+++ apps/mgmt-ui/src/components/ApplicationMenu/index.tsx
@@ -1,8 +1,9 @@
-import { addApplication } from '@/client';
+import { addApplication, deleteApplication, updateApplicationName } from '@/client';
 import { useActiveApplication } from '@/hooks/useActiveApplication';
 import { useApplications } from '@/hooks/useApplications';
+import { MoreVert } from '@mui/icons-material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import { Box, Divider, Typography } from '@mui/material';
+import { Box, Divider, IconButton, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -13,6 +14,7 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
 import { Stack } from '@mui/system';
+import { Application } from '@supaglue/types';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import * as React from 'react';
@@ -20,8 +22,9 @@ import * as React from 'react';
 export default function ApplicationMenu() {
   const router = useRouter();
   const { applications = [], mutate } = useApplications();
-  const { activeApplication, isLoading } = useActiveApplication();
+  const { activeApplication, isLoading, mutate: mutateActiveApplication } = useActiveApplication();
 
+  // Top-level menu
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -36,6 +39,24 @@ export default function ApplicationMenu() {
     const newApplication = await addApplication(name);
     await mutate([...applications, newApplication]);
     await router.push(`/applications/${newApplication.id}`);
+  };
+
+  const onUpdateApplicationName = async (id: string, name: string) => {
+    handleClose();
+    const newApplication = await updateApplicationName(id, name);
+    await mutate(applications.map((application) => (application.id === id ? newApplication : application)));
+    if (id === activeApplication?.id) {
+      await mutateActiveApplication(newApplication);
+    }
+  };
+
+  const onDeleteApplication = async (id: string) => {
+    handleClose();
+    await deleteApplication(id);
+    await mutate(applications.filter((application) => application.id !== id));
+    if (activeApplication?.id === id) {
+      await router.replace('/');
+    }
   };
 
   return (
@@ -74,15 +95,202 @@ export default function ApplicationMenu() {
         }}
       >
         {/* TODO: Implement loading state */}
-        {applications.map(({ id, name }) => (
-          <MenuItem key={id} component={NextLink} href={`/applications/${id}`}>
-            {name}
-          </MenuItem>
+        {applications.map((application) => (
+          <Application
+            key={application.id}
+            application={application}
+            onUpdateApplicationName={onUpdateApplicationName}
+            onDeleteApplication={onDeleteApplication}
+          />
         ))}
         <Divider />
         <NewApplication onCreate={onAddApplication} />
       </Menu>
     </Stack>
+  );
+}
+
+function Application({
+  application,
+  onUpdateApplicationName,
+  onDeleteApplication,
+}: {
+  application: Application;
+  onUpdateApplicationName: (id: string, name: string) => void;
+  onDeleteApplication: (id: string) => void;
+}) {
+  const { id, name } = application;
+
+  // Nested menu
+  const [nestedAnchorEl, setNestedAnchorEl] = React.useState<null | HTMLElement>(null);
+  const handleNestedMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    event.stopPropagation(); // don't click the parent button
+    event.preventDefault();
+    setNestedAnchorEl(event.currentTarget);
+  };
+  const handleNestedMenuClose = () => {
+    setNestedAnchorEl(null);
+  };
+
+  return (
+    <>
+      <MenuItem component={NextLink} href={`/applications/${id}`} sx={{ paddingRight: 0 }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+          <Typography>{name}</Typography>
+          <IconButton onClick={handleNestedMenuOpen}>
+            <MoreVert />
+          </IconButton>
+        </Box>
+      </MenuItem>
+      <Menu
+        anchorEl={nestedAnchorEl}
+        open={Boolean(nestedAnchorEl)}
+        onClose={handleNestedMenuClose}
+        anchorOrigin={{ vertical: 'center', horizontal: 'right' }}
+      >
+        <UpdateApplicationMenuItem application={application} onUpdateApplicationName={onUpdateApplicationName} />
+        <DeleteApplicationMenuItem application={application} onDeleteApplication={onDeleteApplication} />
+      </Menu>
+    </>
+  );
+}
+
+function UpdateApplicationMenuItem({
+  application,
+  onUpdateApplicationName,
+}: {
+  application: Application;
+  onUpdateApplicationName: (id: string, name: string) => void;
+}) {
+  const [applicationName, setApplicationName] = React.useState('');
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setApplicationName('');
+  };
+
+  return (
+    <>
+      <MenuItem onClick={handleClickOpen}>Update</MenuItem>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Update Application Name</DialogTitle>
+        <DialogContent>
+          <DialogContentText component="div">
+            Enter a new name for{' '}
+            <Typography display="inline" sx={{ fontWeight: 'bold' }}>
+              {application.name}
+            </Typography>
+            .
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="applicationName"
+            label="Name"
+            type="text"
+            fullWidth
+            value={applicationName}
+            onKeyDown={(e) => {
+              e.stopPropagation();
+            }}
+            onChange={(e) => {
+              setApplicationName(e.target.value);
+            }}
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button
+            onClick={() => {
+              onUpdateApplicationName(application.id, applicationName);
+              // TODO: error state
+              handleClose();
+            }}
+          >
+            Update
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+function DeleteApplicationMenuItem({
+  application,
+  onDeleteApplication,
+}: {
+  application: Application;
+  onDeleteApplication: (id: string) => void;
+}) {
+  const [applicationName, setApplicationName] = React.useState('');
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setApplicationName('');
+  };
+
+  return (
+    <>
+      <MenuItem onClick={handleClickOpen}>Delete</MenuItem>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Delete Application</DialogTitle>
+        <DialogContent>
+          <DialogContentText component="div">
+            You are about to delete application{' '}
+            <Typography display="inline" sx={{ fontWeight: 'bold' }}>
+              {application.name}
+            </Typography>
+            . Type{' '}
+            <Typography display="inline" sx={{ fontWeight: 'bold' }}>
+              {application.name}
+            </Typography>{' '}
+            below to confirm deletion.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="applicationName"
+            label="Name"
+            type="text"
+            fullWidth
+            value={applicationName}
+            onKeyDown={(e) => {
+              e.stopPropagation();
+            }}
+            onChange={(e) => {
+              setApplicationName(e.target.value);
+            }}
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button
+            disabled={application.name !== applicationName}
+            onClick={() => {
+              onDeleteApplication(application.id);
+              // TODO: error state
+              handleClose();
+            }}
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
