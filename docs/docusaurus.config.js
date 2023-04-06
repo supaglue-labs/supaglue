82fc5ac8c5b5b58ac3e813e9b39315529616dabb
George Xing <george@supaglue.com>
2023-04-05 17:57:07 -0700

    docs: remove long-tail connector pages

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -208,10 +208,6 @@ const config = {
                 href: '/connectors/ms_dynamics_365_sales',
               },
               {
-                label: 'Zendesk Sell',
-                href: '/connectors/zendesk_sell',
-              },
-              {
                 label: 'Zoho CRM',
                 href: '/connectors/zoho_crm',
               },
@@ -220,24 +216,8 @@ const config = {
                 href: '/connectors/copper',
               },
               {
-                label: 'Keap',
-                href: '/connectors/keap',
-              },
-              {
-                label: 'Teamwork CRM',
-                href: '/connectors/teamwork_crm',
-              },
-              {
-                label: 'Freshsales',
-                href: '/connectors/freshsales',
-              },
-              {
                 label: 'ActiveCampaign',
                 href: '/connectors/activecampaign',
-              },
-              {
-                label: 'Capsule',
-                href: '/connectors/capsule',
               },
             ],
           },
