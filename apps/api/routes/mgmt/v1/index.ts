ad63c3daf46246f1bf89131ab7d300cfbe4aa086
Albert Wu <albert@supaglue.com>
2023-04-07 12:32:58 -0700

    chore: move to internal

--- apps/api/routes/mgmt/v1/index.ts
+++ apps/api/routes/mgmt/v1/index.ts
@@ -1,7 +1,5 @@
-import { getDependencyContainer } from '@/dependency_container';
 import { apiKeyHeaderMiddleware } from '@/middleware/api_key';
 import { openapiMiddleware } from '@/middleware/openapi';
-import { snakecaseKeys } from '@supaglue/utils/snakecase';
 import { Router } from 'express';
 import customer from './customer';
 import integration from './integration';
@@ -9,17 +7,10 @@ import syncHistory from './sync_history';
 import syncInfo from './sync_info';
 import webhook from './webhook';
 
-const { connectionAndSyncService } = getDependencyContainer();
-
 export default function init(app: Router): void {
   const v1Router = Router();
 
   v1Router.use(apiKeyHeaderMiddleware);
-  v1Router.post('/_manually_fix_syncs', async (req, res) => {
-    const result = await connectionAndSyncService.manuallyFixTemporalSyncs();
-    return res.status(200).send(snakecaseKeys(result));
-  });
-
   v1Router.use(openapiMiddleware('mgmt'));
 
   customer(v1Router);
