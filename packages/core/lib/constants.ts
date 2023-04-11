f0a0a65d3b82ddf255a3e22a5a58a8cf496abf4d
Ryan Liu <ryan@supaglue.com>
2023-04-11 13:27:44 -0700

    fix: log salesforce errors (#604)

--- packages/core/lib/constants.ts
+++ packages/core/lib/constants.ts
@@ -4,3 +4,12 @@ export const POSTGRES_UPDATE_BATCH_SIZE = 1000;
 export const ORGANIZATION_ID = 'e7070cc8-36e7-43e2-81fc-ad57713cf2d3';
 export const SG_USER_ID = 'd56b851b-5a36-4480-bc43-515d677f46e3';
 export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD ?? 'admin';
+
+export const ASYNC_RETRY_OPTIONS = {
+  // TODO: Don't make this 'forever', so that the activity will actually get heartbeats
+  // and will know that this activity is making progress.
+  forever: true,
+  factor: 2,
+  minTimeout: 1000,
+  maxTimeout: 60 * 1000,
+};
