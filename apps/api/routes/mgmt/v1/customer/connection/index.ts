31334b42f3b11217bec945419f1793ca49b2e47f
Albert Wu <albert@supaglue.com>
2023-04-11 09:34:33 -0700

    feat: mgmt endpoint to create connection

--- apps/api/routes/mgmt/v1/customer/connection/index.ts
+++ apps/api/routes/mgmt/v1/customer/connection/index.ts
@@ -1,5 +1,9 @@
 import { getDependencyContainer } from '@/dependency_container';
+import { Client as HubspotClient } from '@hubspot/api-client';
 import {
+  CreateConnectionPathParams,
+  CreateConnectionRequest,
+  CreateConnectionResponse,
   DeleteConnectionPathParams,
   DeleteConnectionRequest,
   DeleteConnectionResponse,
@@ -12,7 +16,7 @@ import {
 import { snakecaseKeys } from '@supaglue/utils/snakecase';
 import { Request, Response, Router } from 'express';
 
-const { connectionService } = getDependencyContainer();
+const { connectionService, connectionAndSyncService, integrationService } = getDependencyContainer();
 
 export default function init(app: Router): void {
   const connectionRouter = Router();
@@ -29,6 +33,61 @@ export default function init(app: Router): void {
   );
 
   // NOTE: There is no POST /connections since creating a connection is done upon a successful oauth flow
+
+  // TODO: clean this up
+  connectionRouter.post(
+    '/',
+    async (
+      req: Request<CreateConnectionPathParams, CreateConnectionResponse, CreateConnectionRequest>,
+      res: Response<CreateConnectionResponse>
+    ) => {
+      // Get integration first to get category and provider_name
+      const integration = await integrationService.getByIdAndApplicationId(
+        req.body.integration_id,
+        req.supaglueApplication.id
+      );
+
+      if (integration.providerName !== 'hubspot') {
+        throw new Error('Currently only support creating hubspot connections');
+      }
+
+      // TODO: Share this logic with oauth path
+      let remoteId = req.body.credentials.refresh_token;
+
+      const hubspotClient = new HubspotClient();
+      const token = await hubspotClient.oauth.tokensApi.createToken(
+        'refresh_token',
+        undefined,
+        undefined,
+        integration.config.oauth.credentials.oauthClientId,
+        integration.config.oauth.credentials.oauthClientSecret,
+        req.body.credentials.refresh_token
+      );
+      const expiresAt = new Date(Date.now() + token.expiresIn * 1000).toISOString();
+
+      const { hubId } = await hubspotClient.oauth.accessTokensApi.getAccessToken(token.accessToken);
+      remoteId = hubId.toString();
+
+      const connection = await connectionAndSyncService.create({
+        // TODO: don't denormalize this?
+        category: integration.category,
+        providerName: integration.providerName,
+        applicationId: req.supaglueApplication.id,
+        customerId: req.params.customer_id,
+        integrationId: req.body.integration_id,
+        credentials: {
+          type: req.body.credentials.type,
+          accessToken: token.accessToken,
+          refreshToken: req.body.credentials.refresh_token,
+          instanceUrl: req.body.credentials.instance_url,
+          expiresAt,
+        },
+        remoteId,
+      });
+
+      return res.status(200).send(snakecaseKeys(connection));
+    }
+  );
 
   connectionRouter.get(
     '/:connection_id',
