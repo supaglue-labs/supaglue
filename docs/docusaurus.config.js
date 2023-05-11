03e108f0e72a4d3ea6930251ce847837963f3da2
Albert Wu <albert@supaglue.com>
2023-05-11 16:06:15 -0700

    docs: add engagement API docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -75,12 +75,11 @@ const config = {
                 spec: `../openapi/versioned/version-${version}/crm/openapi.bundle.json`,
                 route: version === LATEST_VERSION ? '/references/api/crm' : `/${version}/references/api/crm`,
               },
-              // TODO: Add this
-              // {
-              //   spec: `../openapi/versioned/version-${version}/engagement/openapi.bundle.json`,
-              //   route:
-              //     version === LATEST_VERSION ? '/references/api/engagement' : `/${version}/references/api/engagement`,
-              // },
+              {
+                spec: `../openapi/versioned/version-${version}/engagement/openapi.bundle.json`,
+                route:
+                  version === LATEST_VERSION ? '/references/api/engagement' : `/${version}/references/api/engagement`,
+              },
               {
                 spec: `../openapi/versioned/version-${version}/mgmt/openapi.bundle.json`,
                 route: version === LATEST_VERSION ? '/references/api/mgmt' : `/${version}/references/api/mgmt`,
--- docs/versioned_docs/version-0.8.1/references/api.md
+++ docs/versioned_docs/version-0.8.1/references/api.md
@@ -4,7 +4,8 @@ description: ''
 
 # API Reference
 
-Supaglue supports 2 APIs:
+Supaglue supports 3 APIs:
 
 * [Management API](api/mgmt) can be used to manage customer integrations and connections.
 * [CRM API](api/crm) can be used to read and write data that has been synced into Supaglue from third-party CRM providers.
+* [Engagement API](api/engagement) can be used to read and write data that has been synced into Supaglue from third-party engagement providers.
