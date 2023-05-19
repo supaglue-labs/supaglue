ac096f15497e0b9bd60b381ef5467bef4017d24e
Albert Wu <albert@supaglue.com>
2023-05-19 15:19:12 -0700

    feat: openapi spec for destination management API endpoints

--- apps/api/routes/mgmt/v1/index.ts
+++ apps/api/routes/mgmt/v1/index.ts
@@ -2,6 +2,7 @@ import { apiKeyHeaderMiddleware } from '@/middleware/api_key';
 import { openapiMiddleware } from '@/middleware/openapi';
 import { Router } from 'express';
 import customer from './customer';
+import destination from './destination';
 import forceSync from './force_sync';
 import integration from './integration';
 import syncHistory from './sync_history';
@@ -15,6 +16,7 @@ export default function init(app: Router): void {
   v1Router.use(openapiMiddleware('mgmt'));
 
   customer(v1Router);
+  destination(v1Router);
   integration(v1Router);
   webhook(v1Router);
   syncInfo(v1Router);
