08b4d2f7a5eee5d39e40b3708af4f39a69ce5345
Ryan Liu <ryan@supaglue.com>
2023-05-23 13:52:04 -0700

    fix: build

--- packages/core/keys/engagement/user.ts
+++ packages/core/keys/engagement/user.ts
@@ -1,4 +1,4 @@
-import { SnakecasedKeysEngagementUserWithTenant } from '@supaglue/types/Engagement';
+import { SnakecasedKeysEngagementUserWithTenant } from '@supaglue/types/engagement';
 import { arrayOfAllKeys } from '../util';
 
 export const keysOfSnakecasedEngagementUserWithTenant = arrayOfAllKeys<SnakecasedKeysEngagementUserWithTenant>()([
