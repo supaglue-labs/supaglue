846a00e752d68213224415b5c8b5a55ea652ca15
Albert Wu <albert@supaglue.com>
2023-05-24 15:51:16 -0700

    chore: internal endpoint for sync migration

--- apps/api/routes/internal/v1/index.ts
+++ apps/api/routes/internal/v1/index.ts
@@ -1,3 +1,4 @@
+import { getDependencyContainer } from '@/dependency_container';
 import { internalMiddleware } from '@/middleware/internal';
 import { internalApplicationMiddleware } from '@/middleware/internal_application';
 import { orgHeaderMiddleware } from '@/middleware/org';
@@ -12,12 +13,88 @@ import syncHistory from './sync_history';
 import syncInfo from './sync_info';
 import webhook from './webhook';
 
+const { prisma } = getDependencyContainer();
+
 export default function init(app: Router): void {
   // application routes should not require application header
   const v1ApplicationRouter = Router();
   v1ApplicationRouter.use(internalMiddleware);
 
   v1ApplicationRouter.use(orgHeaderMiddleware);
+
+  v1ApplicationRouter.get('/_migration_connections', async (req, res) => {
+    const connections = await prisma.connection.findMany({
+      where: {
+        integration: {
+          application: {
+            orgId: req.orgId,
+          },
+        },
+      },
+      select: {
+        id: true,
+        providerName: true,
+        customerId: true,
+        sync: {
+          select: {
+            version: true,
+          },
+        },
+      },
+    });
+
+    return res.status(200).send(
+      connections.map((connection) => ({
+        id: connection.id,
+        provider_name: connection.providerName,
+        customer_id: connection.customerId,
+        sync_version: connection.sync?.version,
+      }))
+    );
+  });
+
+  // TODO: Need to reset sync state too
+  v1ApplicationRouter.post('/_migrate_connections', async (req, res) => {
+    const { version } = req.body; // v1 or v2
+    if (version !== 'v1' && version !== 'v2') {
+      throw new Error(`Invalid version: ${version}`);
+    }
+
+    const syncs = await prisma.sync.findMany({
+      where: {
+        connection: {
+          integration: {
+            application: {
+              orgId: req.orgId,
+            },
+          },
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    await prisma.$transaction([
+      prisma.sync.updateMany({
+        data: {
+          version,
+        },
+        where: {
+          id: {
+            in: syncs.map((sync) => sync.id),
+          },
+        },
+      }),
+      prisma.syncChange.createMany({
+        data: syncs.map((sync) => ({
+          syncId: sync.id,
+        })),
+      }),
+    ]);
+
+    return res.status(200).send();
+  });
 
   application(v1ApplicationRouter);
   auth(v1ApplicationRouter);
