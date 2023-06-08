63bd5521c6bd78b3a0ce9bf57860f8dc5db9e3da
tom <tomkit@gmail.com>
2023-06-07 21:38:56 -0700

    fix(mgmt-ui): fix a truthy issue when using auth.js locally

--- apps/mgmt-ui/src/pages/api/index.ts
+++ apps/mgmt-ui/src/pages/api/index.ts
@@ -2,4 +2,4 @@ export const API_HOST = process.env.SUPAGLUE_API_HOST!;
 export const APPLICATION_ID = 'a4398523-03a2-42dd-9681-c91e3e2efaf4';
 export const ORGANIZATION_ID = 'e7070cc8-36e7-43e2-81fc-ad57713cf2d3';
 export const SG_INTERNAL_TOKEN = process.env.SUPAGLUE_INTERNAL_TOKEN!;
-export const IS_CLOUD = !!process.env.NEXT_PUBLIC_IS_CLOUD;
+export const IS_CLOUD = process.env.NEXT_PUBLIC_IS_CLOUD === 'true' || process.env.NEXT_PUBLIC_IS_CLOUD === '1';
