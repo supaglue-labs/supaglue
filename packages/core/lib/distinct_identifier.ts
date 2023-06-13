5dd4661d579bd98d6eeb24ab85e919c5d99d73bc
Lucas Marshall <lucas@supergrain.com>
2023-06-13 14:12:09 -0700

    fix: skip distinct identifier code on cloud

--- packages/core/lib/distinct_identifier.ts
+++ packages/core/lib/distinct_identifier.ts
@@ -3,30 +3,35 @@ import os from 'os';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
-const sgDeploymentId = process.env.SUPAGLUE_DEPLOYMENT_ID;
-const configPath = path.join(os.homedir(), '.supaglue', 'session.json');
-let distinctIdentifier: string | undefined = undefined;
-
 function generateDistinctId() {
+  const sgDeploymentId = process.env.SUPAGLUE_DEPLOYMENT_ID;
   // Note: use coordination by naming convention for distinct ids between different supaglue services in the same deployment. We can add a suffix to avoid collisions down the road
   return sgDeploymentId ? sgDeploymentId : uuidv4();
 }
 
-// read distinctId from config file or write it if it doesn't exist
-if (fs.existsSync(configPath)) {
-  const session = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+let distinctId: string | undefined = undefined;
+if (!process.env.IS_CLOUD) {
+  const configPath = path.join(os.homedir(), '.supaglue', 'session.json');
+  let distinctIdentifier: string | undefined = undefined;
 
-  if (!session.distinctIdentifier) {
-    session.distinctIdentifier = generateDistinctId();
-    fs.writeFileSync(configPath, JSON.stringify(session), 'utf8');
+  // read distinctId from config file or write it if it doesn't exist
+  if (fs.existsSync(configPath)) {
+    const session = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
+    if (!session.distinctIdentifier) {
+      session.distinctIdentifier = generateDistinctId();
+      fs.writeFileSync(configPath, JSON.stringify(session), 'utf8');
+    }
+
+    ({ distinctIdentifier } = session);
+  } else {
+    distinctIdentifier = generateDistinctId();
+    fs.mkdirSync(path.join(os.homedir(), '.supaglue'), { recursive: true });
+    fs.writeFileSync(configPath, JSON.stringify({ distinctIdentifier }));
   }
 
-  ({ distinctIdentifier } = session);
-} else {
-  distinctIdentifier = generateDistinctId();
-  fs.mkdirSync(path.join(os.homedir(), '.supaglue'), { recursive: true });
-  fs.writeFileSync(configPath, JSON.stringify({ distinctIdentifier }));
+  // The distinctIdentifier "session" lives from the time a developer runs docker compose to the time that they delete `session.json` from their home directory on the filesystem. Locally, it persists across docker compose up/down/restart and updates.
+  distinctId = distinctIdentifier && `session:${distinctIdentifier}`;
 }
 
-// The distinctIdentifier "session" lives from the time a developer runs docker compose to the time that they delete `session.json` from their home directory on the filesystem. Locally, it persists across docker compose up/down/restart and updates.
-export const distinctId = process.env.IS_CLOUD ? undefined : distinctIdentifier && `session:${distinctIdentifier}`;
+export { distinctId };
