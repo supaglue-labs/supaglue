cac0c5e945f5457eced0adcb24568e8d12a7fe73
Ryan Liu <ryan@supaglue.com>
2023-06-13 13:55:06 -0700

    fix: re-migrate

--- packages/db/prisma/migrations/20230612233301_sync_config_change/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "sync_config_changes" (
-    "id" TEXT NOT NULL,
-    "sync_config_id" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "sync_config_changes_pkey" PRIMARY KEY ("id")
-);
--- packages/db/prisma/migrations/20230613185129_sync_config_fks/migration.sql
+++
@@ -1,5 +0,0 @@
--- AddForeignKey
-ALTER TABLE "sync_configs" ADD CONSTRAINT "sync_configs_provider_id_fkey" FOREIGN KEY ("provider_id") REFERENCES "providers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "sync_configs" ADD CONSTRAINT "sync_configs_destination_id_fkey" FOREIGN KEY ("destination_id") REFERENCES "destinations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
