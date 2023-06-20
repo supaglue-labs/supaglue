33a1819c11ad4baab7b593a6e43212651e29935f
Ryan Liu <ryan@supaglue.com>
2023-06-20 16:24:49 -0700

    fix: more revivals

---
+++ apps/mgmt-ui/src/pages/api/internal/integrations/index.ts
@@ -0,0 +1,19 @@
+import { getApplicationIdScopedHeaders } from '@/utils/headers';
+import { GetIntegrationsResponse } from '@supaglue/schemas/v2/mgmt';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { API_HOST } from '../..';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<GetIntegrationsResponse | null>) {
+  const result = await fetch(`${API_HOST}/internal/integrations`, {
+    method: 'GET',
+    headers: getApplicationIdScopedHeaders(req),
+  });
+
+  if (!result.ok) {
+    return res.status(500).json(null);
+  }
+
+  const r = await result.json();
+
+  return res.status(200).json(r);
+}
