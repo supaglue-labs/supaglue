7d0a2a67c934f0992dbeed7040753c927c82bebd
Lucas Marshall <lucasmarshall@users.noreply.github.com>
2023-06-21 10:35:54 -0700

    fix: bump body size limit from 100kb to 5mb (#1052)

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -61,7 +61,7 @@ app.use(Sentry.Handlers.requestHandler());
 app.use(expressScopeMiddleware());
 
 // Body parsing Middleware
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 //cors
