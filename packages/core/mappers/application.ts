59740b07042442d061f2b5da9be3b04ffb425ebe
thomas chen <tomkit@gmail.com>
2023-06-21 15:11:48 -0700

    chore: oncall alert cleanup (#1060)
    - add `environment` field to application
    - lower webhook from error to warn
    - warn on sync failures for `environment` === `development` (i'll set
    this for certain customer applications)
    
    ## Test Plan
    
    [Describe test plan here]
    
    ## Deployment instructions
    
    [Add any special deployment instructions here]

--- packages/core/mappers/application.ts
+++ packages/core/mappers/application.ts
@@ -1,10 +1,11 @@
 import type { Application as ApplicationModel } from '@supaglue/db';
 import { Application, ApplicationConfig } from '@supaglue/types';
 
-export const fromApplicationModel = ({ id, name, config, orgId }: ApplicationModel): Application => {
+export const fromApplicationModel = ({ id, name, environment, config, orgId }: ApplicationModel): Application => {
   return {
     id,
     name,
+    environment,
     config: config as ApplicationConfig,
     orgId,
   };
