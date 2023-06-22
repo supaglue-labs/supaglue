93346197130a3aed034bae2c67b4b1daedf6c35c
Lucas Marshall <lucasmarshall@users.noreply.github.com>
2023-06-22 13:20:52 -0700

    chore: add posthog to fe (#1067)

--- apps/mgmt-ui/src/pages/_app.tsx
+++ apps/mgmt-ui/src/pages/_app.tsx
@@ -1,13 +1,28 @@
 import { NotificationManager } from '@/context/notification';
 import Navigator from '@/layout/Navigator';
 import '@/styles/globals.css';
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider, useUser } from '@clerk/nextjs';
 import { Box, CssBaseline, StyledEngineProvider, useMediaQuery } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
-import { ReactNode, useState } from 'react';
+import { useRouter } from 'next/router';
+import posthog from 'posthog-js';
+import { PostHogProvider, usePostHog } from 'posthog-js/react';
+import { ReactNode, useEffect, useState } from 'react';
 import { IS_CLOUD } from './api';
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY!, {
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') {
+        posthog.debug();
+      }
+    },
+  });
+}
 
 // Note: from material-ui template. Eventually consolidate between styled props, sx, and tailwindcss
 export let theme = createTheme({
@@ -156,39 +171,76 @@ theme = {
 const drawerWidth = 256;
 
 export default function App({ Component, pageProps: { session, signedIn, ...pageProps } }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture('$pageview');
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   if (!IS_CLOUD) {
     if (!signedIn) {
       return null;
     }
     return (
-      <SessionProvider session={session}>
-        <StyledEngineProvider injectFirst>
-          <ThemeProvider theme={theme}>
-            <InnerApp signedIn={signedIn}>
-              <NotificationManager>
-                <Component {...pageProps} />
-              </NotificationManager>
-            </InnerApp>
-          </ThemeProvider>
-        </StyledEngineProvider>
-      </SessionProvider>
+      <PostHogProvider client={posthog}>
+        <SessionProvider session={session}>
+          <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+              <InnerApp signedIn={signedIn}>
+                <NotificationManager>
+                  <Component {...pageProps} />
+                </NotificationManager>
+              </InnerApp>
+            </ThemeProvider>
+          </StyledEngineProvider>
+        </SessionProvider>
+      </PostHogProvider>
     );
   }
 
   return (
-    <ClerkProvider {...pageProps}>
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <InnerApp signedIn={signedIn}>
-            <NotificationManager>
-              <Component {...pageProps} />
-            </NotificationManager>
-          </InnerApp>
-        </ThemeProvider>
-      </StyledEngineProvider>
-    </ClerkProvider>
+    <PostHogProvider client={posthog}>
+      <ClerkProvider {...pageProps}>
+        <PosthogClerkUserIdentifier>
+          <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+              <InnerApp signedIn={signedIn}>
+                <NotificationManager>
+                  <Component {...pageProps} />
+                </NotificationManager>
+              </InnerApp>
+            </ThemeProvider>
+          </StyledEngineProvider>
+        </PosthogClerkUserIdentifier>
+      </ClerkProvider>
+    </PostHogProvider>
   );
 }
+
+const PosthogClerkUserIdentifier = (props: { children: ReactNode }) => {
+  const posthog = usePostHog();
+  const { user, isLoaded, isSignedIn } = useUser();
+
+  useEffect(() => {
+    if (isLoaded && isSignedIn) {
+      posthog?.identify(user.id, {
+        email: user.emailAddresses[0]?.emailAddress,
+      });
+      posthog?.group('company', user.organizationMemberships[0]?.organization.id, {
+        name: user.organizationMemberships[0]?.organization.name,
+        date_created: user.organizationMemberships[0]?.organization.createdAt,
+      });
+    }
+  }, [posthog, user, isLoaded, isSignedIn]);
+
+  return <>{props.children}</>;
+};
 
 function InnerApp({ signedIn, children }: { signedIn: boolean; children: ReactNode }) {
   const [mobileOpen, setMobileOpen] = useState(false);
