822b13d6e02fe4bd9f136458309188e4fae57c91
Albert <albert@supaglue.com>
2023-06-22 16:08:31 -0700

    fix: custom objects saving into standard objects (#1089)
    there is also a cache issue that isn't addressed in this PR
    
    ## Test Plan
    
    [Describe test plan here]
    
    ## Deployment instructions
    
    [Add any special deployment instructions here]

--- apps/mgmt-ui/src/components/configuration/syncConfig/SyncConfigDetailsPanel.tsx
+++ apps/mgmt-ui/src/components/configuration/syncConfig/SyncConfigDetailsPanel.tsx
@@ -308,7 +308,7 @@ function SyncConfigDetailsPanelImpl({ syncConfig, isLoading }: SyncConfigDetails
                     />
                   )}
                   onChange={(event: any, value: string[]) => {
-                    setStandardObjects(value);
+                    setCustomObjects(value);
                   }}
                 />
               </Stack>
