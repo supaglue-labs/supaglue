a65e4c363d01bd931949c3ea78d6bead4a6cc36b
Ryan Liu <ryan@supaglue.com>
2023-06-30 16:19:47 -0700

    chore: refactor

--- apps/sync-worker/interceptors/workflow_log_interceptor.ts
+++ apps/sync-worker/interceptors/workflow_log_interceptor.ts
@@ -20,39 +20,12 @@ class WorkflowLogInterceptor implements WorkflowOutboundCallsInterceptor, Workfl
   async execute(input: WorkflowExecuteInput, next: Next<WorkflowInboundCallsInterceptor, 'execute'>): Promise<unknown> {
     const contextArgs = (((input.args[0] ?? {}) as Record<string, unknown>).context ?? {}) as Record<string, unknown>;
 
-    const syncId = contextArgs[TEMPORAL_CONTEXT_ARGS.SYNC_ID];
-    if (syncId) {
-      this.contextHeaders[`sg-${TEMPORAL_CONTEXT_ARGS.SYNC_ID}`] = defaultPayloadConverter.toPayload(syncId);
-    }
-
-    const applicationId = contextArgs[TEMPORAL_CONTEXT_ARGS.APPLICATION_ID];
-    if (applicationId) {
-      this.contextHeaders[`sg-${TEMPORAL_CONTEXT_ARGS.APPLICATION_ID}`] =
-        defaultPayloadConverter.toPayload(applicationId);
-    }
-
-    const customerId = contextArgs[TEMPORAL_CONTEXT_ARGS.CUSTOMER_ID];
-    if (customerId) {
-      this.contextHeaders[`sg-${TEMPORAL_CONTEXT_ARGS.CUSTOMER_ID}`] = defaultPayloadConverter.toPayload(customerId);
-    }
-
-    const connectionId = contextArgs[TEMPORAL_CONTEXT_ARGS.CONNECTION_ID];
-    if (connectionId) {
-      this.contextHeaders[`sg-${TEMPORAL_CONTEXT_ARGS.CONNECTION_ID}`] =
-        defaultPayloadConverter.toPayload(connectionId);
-    }
-
-    const providerCategory = contextArgs[TEMPORAL_CONTEXT_ARGS.PROVIDER_CATEGORY];
-    if (providerCategory) {
-      this.contextHeaders[`sg-${TEMPORAL_CONTEXT_ARGS.PROVIDER_CATEGORY}`] =
-        defaultPayloadConverter.toPayload(providerCategory);
-    }
-
-    const providerName = contextArgs[TEMPORAL_CONTEXT_ARGS.PROVIDER_NAME];
-    if (providerName) {
-      this.contextHeaders[`sg-${TEMPORAL_CONTEXT_ARGS.PROVIDER_NAME}`] =
-        defaultPayloadConverter.toPayload(providerName);
-    }
+    Object.entries(TEMPORAL_CONTEXT_ARGS).forEach(([_, value]) => {
+      const contextKey = `sg-${value}`;
+      if (contextArgs[value]) {
+        this.contextHeaders[contextKey] = defaultPayloadConverter.toPayload(contextArgs[value]);
+      }
+    });
 
     return await next(input);
   }
