c275b4778851b8ee2fd25cff76b83e13073c1f6f
Ryan Liu <ryan@supaglue.com>
2023-07-07 11:36:58 -0700

    fix: mgmt-ui

--- apps/mgmt-ui/src/components/configuration/provider/ProviderDetailsPanel.tsx
+++ apps/mgmt-ui/src/components/configuration/provider/ProviderDetailsPanel.tsx
@@ -60,6 +60,8 @@ export default function ProviderDetailsPanel({ providerName, category, isLoading
 
   const providerCardInfo = providerCardsInfo.find((providerCardInfo) => providerCardInfo.providerName === providerName);
 
+  console.log(`objects: `, commonObjects);
+
   useEffect(() => {
     setFriendlyProviderId(provider?.id ?? '--');
 
@@ -339,7 +341,8 @@ const SchemaToObjectMapping = ({
           <AddIcon />
         </IconButton>
       </Stack>
-      <Stack direction="column" className="gap-2 pl-2">
+      {/* Set key to force re-render on deletions */}
+      <Stack key={objects.length} direction="column" className="gap-2 pl-2">
         {objects.map((object, index) => {
           return (
             <Stack key={index} direction="row" className="gap-2 items-top">
