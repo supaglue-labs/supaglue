794e7fb8615d9a20eb27d94dabbd41e70fad4b49
Ryan Liu <ryan@supaglue.com>
2023-07-21 16:51:22 -0400

    feat: support passthrough for salesloft (#1281)
    ## Test Plan
    
    Tested locally

--- packages/core/remotes/impl/salesloft/index.ts
+++ packages/core/remotes/impl/salesloft/index.ts
@@ -1,20 +1,83 @@
-import type { ConnectionUnsafe, Provider } from '@supaglue/types';
+import type {
+  ConnectionUnsafe,
+  EngagementOauthProvider,
+  Provider,
+  SendPassthroughRequestRequest,
+  SendPassthroughRequestResponse,
+} from '@supaglue/types';
+import axios from 'axios';
+import { REFRESH_TOKEN_THRESHOLD_MS } from '../../../lib';
 import type { ConnectorAuthConfig } from '../../base';
 import { AbstractEngagementRemoteClient } from '../../categories/engagement/base';
 
+type Credentials = {
+  accessToken: string;
+  refreshToken: string;
+  expiresAt: string | null; // ISO string
+  clientId: string;
+  clientSecret: string;
+};
+
 class SalesloftClient extends AbstractEngagementRemoteClient {
-  public constructor() {
-    // TODO: Support baseUrl
-    super('missing-base-url');
+  readonly #credentials: Credentials;
+  readonly #headers: Record<string, string>;
+  readonly #baseURL: string;
+  public constructor(credentials: Credentials) {
+    super('https://api.salesloft.com');
+    this.#baseURL = 'https://api.salesloft.com';
+    this.#credentials = credentials;
+    this.#headers = { Authorization: `Bearer ${this.#credentials.accessToken}` };
   }
 
   protected override getAuthHeadersForPassthroughRequest(): Record<string, string> {
-    throw new Error('Not implemented');
+    return this.#headers;
+  }
+
+  private async maybeRefreshAccessToken(): Promise<void> {
+    if (
+      !this.#credentials.expiresAt ||
+      Date.parse(this.#credentials.expiresAt) < Date.now() + REFRESH_TOKEN_THRESHOLD_MS
+    ) {
+      const response = await axios.post<{ refresh_token: string; access_token: string; expires_in: number }>(
+        `${authConfig.tokenHost}${authConfig.tokenPath}`,
+        {
+          client_id: this.#credentials.clientId,
+          client_secret: this.#credentials.clientSecret,
+          grant_type: 'refresh_token',
+          refresh_token: this.#credentials.refreshToken,
+        }
+      );
+
+      const newAccessToken = response.data.access_token;
+      const newRefreshToken = response.data.refresh_token;
+      const newExpiresAt = new Date(Date.now() + response.data.expires_in * 1000).toISOString();
+
+      this.#credentials.accessToken = newAccessToken;
+      this.#credentials.refreshToken = newRefreshToken;
+      this.#credentials.expiresAt = newExpiresAt;
+
+      this.emit('token_refreshed', {
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken,
+        expiresAt: newExpiresAt,
+      });
+    }
+  }
+
+  public override async sendPassthroughRequest(
+    request: SendPassthroughRequestRequest
+  ): Promise<SendPassthroughRequestResponse> {
+    await this.maybeRefreshAccessToken();
+    return await super.sendPassthroughRequest(request);
   }
 }
 
 export function newClient(connection: ConnectionUnsafe<'salesloft'>, provider: Provider): SalesloftClient {
-  return new SalesloftClient();
+  return new SalesloftClient({
+    ...connection.credentials,
+    clientId: (provider as EngagementOauthProvider).config.oauth.credentials.oauthClientId,
+    clientSecret: (provider as EngagementOauthProvider).config.oauth.credentials.oauthClientSecret,
+  });
 }
 
 // TODO: support other geographies
