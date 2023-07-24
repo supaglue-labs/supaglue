d27dffaeabb469fc8160b13e64739be7eb52dca2
Albert Wu <albert@supaglue.com>
2023-07-24 14:14:12 -0700

    fix: type

--- apps/api/routes/mgmt/v2/object/index.ts
+++ apps/api/routes/mgmt/v2/object/index.ts
@@ -26,7 +26,7 @@ export default function init(app: Router): void {
       if (req.customerConnection.category !== 'crm') {
         throw new BadRequestError('Only CRM connections are supported for this operation');
       }
-      const client = await remoteService.getCrmRemoteClient(req.customerConnection.id);
+      const [client] = await remoteService.getCrmRemoteClient(req.customerConnection.id);
       const objects = await client.listObjects();
       return res.status(200).send({ objects: objects.map(snakecaseKeys) });
     }
