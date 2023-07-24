c599b3354d01856687691a19eed76bbf7b737bad
Albert <albert@supaglue.com>
2023-07-24 11:53:42 -0700

    chore: return all entities in /entity_mappings (#1288)
    ## Test Plan
    
    [Describe test plan here]
    
    ## Deployment instructions
    
    [Add any special deployment instructions here]

--- packages/core/mappers/entity_mapping.ts
+++ packages/core/mappers/entity_mapping.ts
@@ -1,134 +1,108 @@
+import type { Entity } from '@supaglue/types/entity';
 import type { EntityMapping, MergedEntityMapping } from '@supaglue/types/entity_mapping';
 
 export function mergeEntityMappings(
   providerEntityMapping: EntityMapping | undefined,
   connectionEntityMapping: EntityMapping | undefined
-): MergedEntityMapping | undefined {
-  if (providerEntityMapping) {
-    if (connectionEntityMapping) {
-      if (providerEntityMapping.entityId !== connectionEntityMapping.entityId) {
-        throw new Error('Entity mappings must be for the same entity');
-      }
+): EntityMapping | undefined {
+  if (
+    providerEntityMapping &&
+    connectionEntityMapping &&
+    providerEntityMapping.entityId !== connectionEntityMapping.entityId
+  ) {
+    throw new Error('Entity mappings must be for the same entity');
+  }
 
-      const mergedFieldMappings: MergedEntityMapping['fieldMappings'] = providerEntityMapping.fieldMappings?.map(
-        (mapping) => ({
-          ...mapping,
-          from: 'developer',
-        })
-      );
+  let mergedEntityMapping: EntityMapping | undefined = undefined;
 
-      for (const mapping of connectionEntityMapping.fieldMappings ?? []) {
-        const existingMapping = mergedFieldMappings?.find((m) => m.entityField === mapping.entityField);
+  for (const entityMapping of [providerEntityMapping, connectionEntityMapping]) {
+    if (!entityMapping) {
+      continue;
+    }
+
+    if (!mergedEntityMapping) {
+      mergedEntityMapping = {
+        entityId: entityMapping.entityId,
+        object: entityMapping.object,
+        fieldMappings: entityMapping.fieldMappings,
+      };
+    } else {
+      mergedEntityMapping.object = entityMapping.object;
+
+      for (const mapping of entityMapping.fieldMappings ?? []) {
+        const existingMapping = mergedEntityMapping.fieldMappings?.find((m) => m.entityField === mapping.entityField);
         if (existingMapping) {
           existingMapping.mappedField = mapping.mappedField;
-          existingMapping.from = 'customer';
         } else {
-          mergedFieldMappings?.push({
-            ...mapping,
-            from: 'customer',
-          });
+          if (mergedEntityMapping.fieldMappings) {
+            mergedEntityMapping.fieldMappings.push(mapping);
+          } else {
+            mergedEntityMapping.fieldMappings = [mapping];
+          }
         }
       }
-
-      return {
-        entityId: providerEntityMapping.entityId,
-        object: connectionEntityMapping.object
-          ? {
-              ...connectionEntityMapping.object,
-              from: 'customer',
-            }
-          : providerEntityMapping.object
-          ? {
-              ...providerEntityMapping.object,
-              from: 'developer',
-            }
-          : undefined,
-        fieldMappings: mergedFieldMappings,
-      };
-    } else {
-      return {
-        entityId: providerEntityMapping.entityId,
-        object: providerEntityMapping.object
-          ? {
-              ...providerEntityMapping.object,
-              from: 'developer',
-            }
-          : undefined,
-        fieldMappings: providerEntityMapping.fieldMappings?.map((mapping) => ({
-          ...mapping,
-          from: 'developer',
-        })),
-      };
-    }
-  } else {
-    if (connectionEntityMapping) {
-      return {
-        entityId: connectionEntityMapping.entityId,
-        object: connectionEntityMapping.object
-          ? {
-              ...connectionEntityMapping.object,
-              from: 'customer',
-            }
-          : undefined,
-        fieldMappings: connectionEntityMapping.fieldMappings?.map((mapping) => ({
-          ...mapping,
-          from: 'customer',
-        })),
-      };
-    } else {
-      return undefined;
     }
   }
+
+  return mergedEntityMapping;
 }
 
+/**
+ * Given all the entities, provider entity mappings, and connection entity mappings,
+ * return a list of MergedEntityMapping that contains all entities, and the associated merged
+ * entity mappings for the entities that indeed have some mapping.
+ */
 export function mergeEntityMappingsList(
+  entities: Entity[],
   providerEntityMappings: EntityMapping[],
   connectionEntityMappings: EntityMapping[]
 ): MergedEntityMapping[] {
-  const mergedEntityMappings: MergedEntityMapping[] = [];
+  // Start off with a skeleton
+  const mergedEntityMappings: MergedEntityMapping[] = entities.map((entity) => ({
+    entityId: entity.id,
+    entityName: entity.name,
+    fieldMappings: entity.config.fields.map((field) => ({
+      entityField: field.name,
+    })),
+  }));
 
-  for (const providerEntityMapping of providerEntityMappings) {
-    const connectionEntityMapping = connectionEntityMappings.find(
-      (mapping) => mapping.entityId === providerEntityMapping.entityId
-    );
-    if (connectionEntityMapping) {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      mergedEntityMappings.push(mergeEntityMappings(providerEntityMapping, connectionEntityMapping)!);
-    } else {
-      mergedEntityMappings.push({
-        entityId: providerEntityMapping.entityId,
-        object: providerEntityMapping.object
-          ? {
-              ...providerEntityMapping.object,
-              from: 'developer',
-            }
-          : undefined,
-        fieldMappings: providerEntityMapping.fieldMappings?.map((mapping) => ({
-          ...mapping,
-          from: 'developer',
-        })),
-      });
-    }
-  }
+  for (const [entityMappings, from] of [
+    [providerEntityMappings, 'developer'] as const,
+    [connectionEntityMappings, 'customer'] as const,
+  ]) {
+    for (const mapping of entityMappings) {
+      const existingEntityMapping = mergedEntityMappings.find((m) => m.entityId === mapping.entityId);
+      // TODO: we should probably remove entity mappings if an entity gets deleted
+      if (!existingEntityMapping) {
+        continue;
+      }
 
-  for (const connectionEntityMapping of connectionEntityMappings) {
-    const providerEntityMapping = providerEntityMappings.find(
-      (mapping) => mapping.entityId === connectionEntityMapping.entityId
-    );
-    if (!providerEntityMapping) {
-      mergedEntityMappings.push({
-        entityId: connectionEntityMapping.entityId,
-        object: connectionEntityMapping.object
-          ? {
-              ...connectionEntityMapping.object,
-              from: 'customer',
-            }
-          : undefined,
-        fieldMappings: connectionEntityMapping.fieldMappings?.map((mapping) => ({
-          ...mapping,
-          from: 'customer',
-        })),
-      });
+      existingEntityMapping.object = mapping.object
+        ? {
+            ...mapping.object,
+            from,
+          }
+        : undefined;
+
+      if (mapping.fieldMappings) {
+        for (const fieldMapping of mapping.fieldMappings) {
+          const existingFieldMapping = existingEntityMapping.fieldMappings.find(
+            (m) => m.entityField === fieldMapping.entityField
+          );
+          if (existingFieldMapping) {
+            existingFieldMapping.mappedField = fieldMapping.mappedField;
+            existingFieldMapping.from = from;
+          } else {
+            // TODO: Only allow this if `allowAdditionalFieldMappings` is true
+            // But if we do that, then we hide additional field mappings that
+            // have been defined, which may cause issues...
+            existingEntityMapping.fieldMappings.push({
+              ...fieldMapping,
+              from,
+            });
+          }
+        }
+      }
     }
   }
 
