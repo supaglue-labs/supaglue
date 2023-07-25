60b346b2f2703edaa02df7d56aef25ca351cb1b0
Ryan Liu <ryan@supaglue.com>
2023-07-25 14:26:21 -0700

    feat: more provider cards

--- apps/mgmt-ui/src/utils/companyToIcon.tsx
+++ apps/mgmt-ui/src/utils/companyToIcon.tsx
@@ -1,14 +1,32 @@
 import ActiveCampaignIcon from '@/assets/connector_icons/activecampaign.png';
 import ApolloIcon from '@/assets/connector_icons/apollo.webp';
+import AsanaIcon from '@/assets/connector_icons/asana.png';
+import BoxIcon from '@/assets/connector_icons/box.png';
 import CopperIcon from '@/assets/connector_icons/copper.png';
+import DropboxIcon from '@/assets/connector_icons/dropbox.png';
+import GCalIcon from '@/assets/connector_icons/gcal.png';
+import GmailIcon from '@/assets/connector_icons/gmail.png';
 import GongIcon from '@/assets/connector_icons/gong.webp';
+import GDriveIcon from '@/assets/connector_icons/google_drive.png';
 import HubspotIcon from '@/assets/connector_icons/hubspot.png';
+import IntercomIcon from '@/assets/connector_icons/intercom.png';
+import LinkedInIcon from '@/assets/connector_icons/linkedin.png';
+import MarketoIcon from '@/assets/connector_icons/marketo.png';
+import MessengerIcon from '@/assets/connector_icons/messenger.png';
 import MicrosoftDynamics365SalesIcon from '@/assets/connector_icons/ms_dynamics_365_sales.svg';
+import TeamsIcon from '@/assets/connector_icons/ms_teams.png';
+import OneDriveIcon from '@/assets/connector_icons/onedrive.png';
+import OutlookIcon from '@/assets/connector_icons/outlook.png';
 import OutreachIcon from '@/assets/connector_icons/outreach.png';
+import PardotIcon from '@/assets/connector_icons/pardot.png';
 import PipedriveIcon from '@/assets/connector_icons/pipedrive.png';
 import SalesforceIcon from '@/assets/connector_icons/salesforce.png';
 import SalesloftIcon from '@/assets/connector_icons/salesloft.webp';
+import SlackIcon from '@/assets/connector_icons/slack.png';
+import WhatsappIcon from '@/assets/connector_icons/whatsapp.png';
+import ZendeskIcon from '@/assets/connector_icons/zendesk.png';
 import ZendeskSellIcon from '@/assets/connector_icons/zendesk_sell.png';
+import ZohoIcon from '@/assets/connector_icons/zoho_crm.png';
 import BigQueryIcon from '@/assets/destination_icons/bigquery.png';
 import MongoDBIcon from '@/assets/destination_icons/mongodb.png';
 import PostgresIcon from '@/assets/destination_icons/postgres.png';
@@ -36,6 +54,24 @@ export default function getIcon(name: string, size = 25): ReactNode {
     gong: <Image key={name} alt={name} src={GongIcon} width={size} height={size} />,
     apollo: <Image key={name} alt={name} src={ApolloIcon} width={size} height={size} />,
     salesloft: <Image key={name} alt={name} src={SalesloftIcon} width={size} height={size} />,
+    asana: <Image key={name} alt={name} src={AsanaIcon} width={size} height={size} />,
+    box: <Image key={name} alt={name} src={BoxIcon} width={size} height={size} />,
+    dropbox: <Image key={name} alt={name} src={DropboxIcon} width={size} height={size} />,
+    google_calendar: <Image key={name} alt={name} src={GCalIcon} width={size} height={size} />,
+    gmail: <Image key={name} alt={name} src={GmailIcon} width={size} height={size} />,
+    google_drive: <Image key={name} alt={name} src={GDriveIcon} width={size} height={size} />,
+    intercom: <Image key={name} alt={name} src={IntercomIcon} width={size} height={size} />,
+    linkedin: <Image key={name} alt={name} src={LinkedInIcon} width={size} height={size} />,
+    marketo: <Image key={name} alt={name} src={MarketoIcon} width={size} height={size} />,
+    messenger: <Image key={name} alt={name} src={MessengerIcon} width={size} height={size} />,
+    ms_teams: <Image key={name} alt={name} src={TeamsIcon} width={size} height={size} />,
+    onedrive: <Image key={name} alt={name} src={OneDriveIcon} width={size} height={size} />,
+    outlook: <Image key={name} alt={name} src={OutlookIcon} width={size} height={size} />,
+    pardot: <Image key={name} alt={name} src={PardotIcon} width={size} height={size} />,
+    slack: <Image key={name} alt={name} src={SlackIcon} width={size} height={size} />,
+    whatsapp: <Image key={name} alt={name} src={WhatsappIcon} width={size} height={size} />,
+    zendesk: <Image key={name} alt={name} src={ZendeskIcon} width={size} height={size} />,
+    zoho: <Image key={name} alt={name} src={ZohoIcon} width={size} height={size} />,
     supaglue: <Image key={name} alt={name} src={SupaglueIcon} width={size} height={size} />,
   };
 
