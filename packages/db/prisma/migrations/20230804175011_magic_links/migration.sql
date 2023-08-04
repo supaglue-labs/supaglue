b4ff80323cde82474af53bff34f73d8c9d1c822b
Ryan Liu <ryan@supaglue.com>
2023-08-04 10:51:47 -0700

    fix: missing migration

---
+++ packages/db/prisma/migrations/20230804175011_magic_links/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "magic_links" (
+    "id" TEXT NOT NULL,
+    "application_id" TEXT NOT NULL,
+    "customer_id" TEXT NOT NULL,
+    "provider_id" TEXT NOT NULL,
+    "provider_name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "url" TEXT NOT NULL,
+    "return_url" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "magic_links_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "magic_links" ADD CONSTRAINT "magic_links_application_id_fkey" FOREIGN KEY ("application_id") REFERENCES "applications"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "magic_links" ADD CONSTRAINT "magic_links_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "magic_links" ADD CONSTRAINT "magic_links_provider_id_fkey" FOREIGN KEY ("provider_id") REFERENCES "providers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
