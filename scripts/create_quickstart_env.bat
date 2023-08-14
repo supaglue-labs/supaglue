d83bc7c35fe68e947520967d4074fb14520531c3
Ryan Liu <ryan@supaglue.com>
2023-08-14 14:10:35 -0700

    chore: also bat

--- scripts/create_quickstart_env.bat
+++ scripts/create_quickstart_env.bat
@@ -8,4 +8,11 @@ echo Downloading file from %url%...
 
 powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%output%')"
 
+set url=https://d379ao5oasu7j7.cloudfront.net/mgmt-ui-quickstart.env
+set output=apps/mgmt-ui/.env
+
+echo Downloading file from %url%...
+
+powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%output%')"
+
 echo Done!
