5379ed1beefffd655966b9cd98e4e93399fb9a76
Ryan Liu <ryan@supaglue.com>
2023-08-14 14:09:32 -0700

    chore: update quickstart to also copy mgmt-ui env vars

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -4,5 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cp docker-compose.override.dev.yml docker-compose.override.yml
 brew install bufbuild/buf/buf
-cp apps/mgmt-ui/.env.sample apps/mgmt-ui/.env
 "$DIR"/create_quickstart_env.sh
