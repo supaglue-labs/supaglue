9943c2f96832a0d78370cd16eb4f3c326f259aaa
Ryan Liu <ryan@supaglue.com>
2023-08-14 14:24:40 -0700

    chore: update quickstart to also copy mgmt-ui .env vars (#1428)

--- scripts/create_quickstart_env.sh
+++ scripts/create_quickstart_env.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
 
 curl -s https://d379ao5oasu7j7.cloudfront.net/quickstart.env > "$ROOT_DIR/.env"
+curl -s https://d379ao5oasu7j7.cloudfront.net/mgmt-ui-quickstart.env > "$ROOT_DIR/apps/mgmt-ui/.env"
 
 echo ".env created!"
