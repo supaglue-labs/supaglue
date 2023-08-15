945361d3611a13f99d21e9abe686e98a0ee2ba1d
Ryan Liu <ryan@supaglue.com>
2023-08-15 16:01:33 -0700

    docs: re-organize actions API hierarchy (#1443)
    ![Screenshot 2023-08-15 at 2 19 20
    PM](https://github.com/supaglue-labs/supaglue/assets/1498449/db9c158a-c630-40b5-acb4-37c0abe88362)
    
    ---------
    
    Co-authored-by: Lucas Marshall <lucas@supergrain.com>

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -242,16 +242,62 @@ const sidebars = {
       type: 'html',
       value: sidebarHeader('Actions API'),
     },
+    { type: 'doc', id: 'api/v2/actions/actions-api' },
     {
       type: 'category',
-      label: 'Entities/Objects',
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      items: require('./docs/api/v2/actions/sidebar.js'),
+      label: 'Entity Records',
+      link: { type: 'doc', id: 'api/v2/actions/entity-records' },
+      collapsed: true,
+      items: [
+        {
+          type: 'doc',
+          id: 'api/v2/actions/create-entity-record',
+          label: 'Create Entity record',
+          className: 'api-method post',
+        },
+        {
+          type: 'doc',
+          id: 'api/v2/actions/get-entity-record',
+          label: 'Get Entity Record',
+          className: 'api-method get',
+        },
+        {
+          type: 'doc',
+          id: 'api/v2/actions/update-entity-record',
+          label: 'Update entity record',
+          className: 'api-method patch',
+        },
+      ],
     },
     {
       type: 'category',
-      label: 'Common Schema',
+      label: 'Object Records',
+      link: { type: 'doc', id: 'api/v2/actions/object-records' },
+      collapsed: true,
+      items: [
+        {
+          type: 'doc',
+          id: 'api/v2/actions/create-standard-object-record',
+          label: 'Create Standard Object record',
+          className: 'api-method post',
+        },
+        {
+          type: 'doc',
+          id: 'api/v2/actions/get-standard-object-record',
+          label: 'Get Standard Object record',
+          className: 'api-method get',
+        },
+        {
+          type: 'doc',
+          id: 'api/v2/actions/update-standard-object-record',
+          label: 'Update Standard Object record',
+          className: 'api-method patch',
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Common Schema Records',
       items: [
         {
           type: 'category',
@@ -269,6 +315,20 @@ const sidebars = {
         },
       ],
     },
+    {
+      type: 'category',
+      label: 'Associations',
+      collapsed: true,
+      items: [
+        { type: 'doc', id: 'api/v2/actions/get-associations', label: 'List associations', className: 'api-method get' },
+        {
+          type: 'doc',
+          id: 'api/v2/actions/create-association',
+          label: 'Create association',
+          className: 'api-method put',
+        },
+      ],
+    },
 
     // section
     {
@@ -276,9 +336,25 @@ const sidebars = {
       value: sidebarHeader('Passthrough API'),
     },
     {
-      id: 'api/v2/actions/send-passthrough-request',
-      label: 'Passthrough API',
+      type: 'category',
+      label: 'Passthrough',
+      link: { type: 'doc', id: 'api/v2/actions/passthrough' },
+      collapsed: true,
+      items: [
+        {
+          type: 'doc',
+          id: 'api/v2/actions/send-passthrough-request',
+          label: 'Send passthrough request',
+          className: 'api-method post',
+        },
+      ],
+      className: 'hidden',
+    },
+    {
       type: 'doc',
+      id: 'api/v2/actions/send-passthrough-request',
+      label: 'Send passthrough request',
+      className: 'api-method post',
     },
   ],
 };
