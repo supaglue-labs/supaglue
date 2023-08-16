f33a1d205aed7513a43ac6efc28ded9d61901d15
Lucas Marshall <lucas@supergrain.com>
2023-08-15 17:47:18 -0700

    fix: make fetchers throw errors on non-200 responses

--- apps/mgmt-ui/src/hooks/index.ts
+++ apps/mgmt-ui/src/hooks/index.ts
@@ -1,5 +1,8 @@
 export const fetcher = async <T>(input: RequestInfo, init: RequestInit): Promise<T> => {
   const res = await fetch(input, init);
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
   const result: T = await res.json();
   return result;
 };
@@ -15,6 +18,9 @@ export const fetcherWithApplication = async <T>(
   supaglueHeaders.append('x-application-id', applicationId);
 
   const res = await fetch(path, { headers: supaglueHeaders, ...init });
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
   const result = await res.json();
   if (transform) {
     return transform(result);
