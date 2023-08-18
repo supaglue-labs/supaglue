9dfca280a124ae62046f8b5a7d2651bf6bea7454
Lucas Marshall <lucas@supergrain.com>
2023-08-17 17:11:08 -0700

    fix: normalize table names

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './camelcase';
 export * from './constants';
+export * from './slugify';
 export * from './snakecase';
--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -5,3 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cp docker-compose.override.dev.yml docker-compose.override.yml
 brew install bufbuild/buf/buf
 "$DIR"/create_quickstart_env.sh
+
+echo "SUPAGLUE_DATABASE_URL=postgres://postgres:supaglue@localhost:5432/postgres?schema=api" > $DIR/../packages/db/.env
+
+echo "Done! Run 'docker-compose up' to start the dev environment."
