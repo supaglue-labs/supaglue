e01d765c5e5b5bfc430d653603608928eee09c11
tom <tomkit@gmail.com>
2023-09-07 16:33:13 -0700

    fix: return raw hubspot response for data listing

--- apps/api/mappers/hubspot.ts
+++ apps/api/mappers/hubspot.ts
@@ -2,26 +2,27 @@ import type { HubspotCompany, HubspotContact } from '@/types/hubspot';
 import type { SupaglueRecord } from '@supaglue/core/lib';
 
 export const toHubspotCompany = (record: SupaglueRecord): HubspotCompany => {
-  const rawData = record._supaglue_raw_data.properties as Record<string, unknown>;
+  const rawData = record._supaglue_raw_data;
+  const properties = rawData.properties as Record<string, unknown>;
 
   return {
     id: record._supaglue_id,
     is_deleted: record._supaglue_is_deleted,
-    description: rawData['description'] as string | null,
-    city: rawData['city'] as string | null,
-    country: rawData['country'] as string | null,
-    zip: rawData['zip'] as string | null,
-    state: rawData['state'] as string | null,
-    address: rawData['address'] as string | null,
-    phone: rawData['phone'] as string | null,
-    industry: rawData['industry'] as string | null,
-    name: rawData['name'] as string | null,
-    numberofemployees: rawData['numberofemployees'] as string | null,
-    hubspot_owner_id: rawData['hubspot_owner_id'] as string | null,
-    domain: rawData['domain'] as string | null,
-    website: rawData['website'] as string | null,
-    createdate: new Date(rawData['createdate'] as string),
-    notes_last_updated: new Date(rawData['notes_last_updated'] as string),
+    description: properties['description'] as string | null,
+    city: properties['city'] as string | null,
+    country: properties['country'] as string | null,
+    zip: properties['zip'] as string | null,
+    state: properties['state'] as string | null,
+    address: properties['address'] as string | null,
+    phone: properties['phone'] as string | null,
+    industry: properties['industry'] as string | null,
+    name: properties['name'] as string | null,
+    numberofemployees: properties['numberofemployees'] as string | null,
+    hubspot_owner_id: properties['hubspot_owner_id'] as string | null,
+    domain: properties['domain'] as string | null,
+    website: properties['website'] as string | null,
+    createdate: new Date(properties['createdate'] as string),
+    notes_last_updated: new Date(properties['notes_last_updated'] as string),
     // TODO: second address
     // TODO: system timestamp
     raw_data: rawData,
@@ -29,27 +30,28 @@ export const toHubspotCompany = (record: SupaglueRecord): HubspotCompany => {
 };
 
 export const toHubspotContact = (record: SupaglueRecord): HubspotContact => {
-  const rawData = record._supaglue_raw_data.properties as Record<string, unknown>;
+  const rawData = record._supaglue_raw_data;
+  const properties = rawData.properties as Record<string, unknown>;
 
   return {
     id: record._supaglue_id,
-    email: rawData['email'] as string | null,
-    associatedcompanyid: rawData['associatedcompanyid'] as string | null,
-    firstname: rawData['firstname'] as string | null,
-    phone: rawData['phone'] as string | null,
+    email: properties['email'] as string | null,
+    associatedcompanyid: properties['associatedcompanyid'] as string | null,
+    firstname: properties['firstname'] as string | null,
+    phone: properties['phone'] as string | null,
     is_deleted: record._supaglue_is_deleted,
-    lastname: rawData['lastname'] as string | null,
-    city: rawData['city'] as string | null,
-    country: rawData['country'] as string | null,
-    zip: rawData['zip'] as string | null,
-    state: rawData['state'] as string | null,
-    address: rawData['address'] as string | null,
-    mobilephone: rawData['mobilephone'] as string | null,
-    hubspot_owner_id: rawData['hubspot_owner_id'] as string | null,
-    fax: rawData['fax'] as string | null,
-    jobtitle: rawData['jobtitle'] as string | null,
-    createdate: new Date(rawData['createdate'] as string),
-    notes_last_updated: new Date(rawData['notes_last_updated'] as string),
+    lastname: properties['lastname'] as string | null,
+    city: properties['city'] as string | null,
+    country: properties['country'] as string | null,
+    zip: properties['zip'] as string | null,
+    state: properties['state'] as string | null,
+    address: properties['address'] as string | null,
+    mobilephone: properties['mobilephone'] as string | null,
+    hubspot_owner_id: properties['hubspot_owner_id'] as string | null,
+    fax: properties['fax'] as string | null,
+    jobtitle: properties['jobtitle'] as string | null,
+    createdate: new Date(properties['createdate'] as string),
+    notes_last_updated: new Date(properties['notes_last_updated'] as string),
     // TODO: description
     // TODO: lead source
     // TODO: second address
