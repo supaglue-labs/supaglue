f2aea4816c74fd68109b93394f123319d07ffbf9
Ryan Liu <ryan@supaglue.com>
2023-09-14 17:12:47 -0700

    fix: remove s3 FE stuff

--- apps/mgmt-ui/src/components/connectors/destination/DestinationDetailsPanel.tsx
+++ apps/mgmt-ui/src/components/connectors/destination/DestinationDetailsPanel.tsx
@@ -1,17 +1,14 @@
 import BigQueryDestinationDetailsPanel from './BigQueryDestinationDetailsPanel';
 import MongoDBDestinationDetailsPanel from './MongoDBDestinationDetailsPanel';
 import PostgresDestinationDetailsPanel from './PostgresDestinationDetailsPanel';
-import S3DestinationDetailsPanel from './S3DestinationDetailsPanel';
 
 export type DestinationDetailsPanelProps = {
-  type: 's3' | 'postgres' | 'bigquery' | 'mongodb';
+  type: 'postgres' | 'bigquery' | 'mongodb';
   isLoading: boolean;
 };
 
 export default function DestinationDetailsPanel({ type, isLoading }: DestinationDetailsPanelProps) {
   switch (type) {
-    case 's3':
-      return <S3DestinationDetailsPanel isLoading={isLoading} />;
     case 'postgres':
       return <PostgresDestinationDetailsPanel isLoading={isLoading} />;
     case 'bigquery':
