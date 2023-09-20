1b3dfbe830a0d4684cbeefbbc97a098d96587d37
tom <tomkit@gmail.com>
2023-09-19 23:33:15 -0700

    fix!: change sync run id pk, drop id col

---
+++ packages/db/prisma/migrations/20230920062058_object_sync_run_drop_old_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `id` on the `object_sync_runs` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "object_sync_runs" DROP COLUMN "id";
