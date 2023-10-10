0363481dae4881780bf34b4014ce8a22734beced
Ryan Liu <ryan@supaglue.com>
2023-10-10 14:55:30 -0700

    fix: missing files

---
+++ apps/mgmt-ui/src/pages/api/internal/api_keys/create_temporary.ts
@@ -0,0 +1,18 @@
+import { getApplicationIdScopedHeaders } from '@/utils/headers';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { API_HOST } from '../..';
+
+// Note: we currently don't define api key endpoints (it's the only one is used internally with not public api)
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const result = await fetch(`${API_HOST}/internal/api_keys/_generate_temporary_api_key`, {
+    method: 'POST',
+    headers: getApplicationIdScopedHeaders(req),
+  });
+
+  const r = await result.json();
+  if (!result.ok) {
+    return res.status(result.status).json(r);
+  }
+
+  return res.status(200).json(r);
+}
