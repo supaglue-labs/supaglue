235aa8e2e46f2ce42c44b2dbd37db619fd7585cd
Ryan Liu <ryan@supaglue.com>
2023-10-10 16:36:51 -0700

    chore: add passthrough context to CRM passthrough as well

--- apps/api/routes/engagement/v2/passthrough.ts
+++ apps/api/routes/engagement/v2/passthrough.ts
@@ -1,4 +1,5 @@
 import { getDependencyContainer } from '@/dependency_container';
+import { addLogContext } from '@supaglue/core/lib/logger';
 import type {
   SendPassthroughRequestPathParams,
   SendPassthroughRequestRequest,
@@ -19,6 +20,12 @@ export default function init(app: Router): void {
       req: Request<SendPassthroughRequestPathParams, SendPassthroughRequestResponse, SendPassthroughRequestRequest>,
       res: Response<SendPassthroughRequestResponse>
     ) => {
+      addLogContext('passthrough', {
+        method: req.body.method,
+        path: req.body.path,
+        headers: req.body.headers,
+        query: req.body.query,
+      });
       const response = await passthroughService.send(req.customerConnection.id, req.body);
       return res.status(200).send(response);
     }
