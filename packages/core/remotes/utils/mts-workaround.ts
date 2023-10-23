7381763934d933eeca4bc2239679cfb5dd374314
Tony Xiao <tonyx.ca@gmail.com>
2023-10-23 16:22:47 -0700

    fix: staging build broken due to importing untranspiled module

--- packages/core/remotes/utils/mts-workaround.ts
+++
@@ -1,4 +0,0 @@
-// @ts-expect-error The current file is a CommonJS module whose imports will produce 'require' calls; however, the referenced file is an ECMAScript module and cannot be imported with 'require'. Consider writing a dynamic 'import("./client.mjs")' call instead.
-import { api as defineApi } from 'zodios-api-shorthand';
-
-export { defineApi };
