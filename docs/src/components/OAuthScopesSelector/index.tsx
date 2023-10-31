cf8dae9e622e36a20018a4a59895bb7e7e8aea52
George Xing <george@supaglue.com>
2023-10-30 19:07:12 -0700

    docs: fix scopes rendering

--- docs/src/components/OAuthScopesSelector/index.tsx
+++ docs/src/components/OAuthScopesSelector/index.tsx
@@ -12,8 +12,15 @@ function ScopesPicker() {
         'crm.lists': ['Read', 'Write'],
         'crm.objects.deals': ['Read', 'Write'],
         'crm.objects.contacts': ['Read', 'Write'],
+        'crm.objects.quotes': ['Read', 'Write'],
+        'crm.objects.line_items': ['Read', 'Write'],
         'crm.objects.custom': ['Read', 'Write'],
         'crm.schemas.custom': ['Read', 'Write'],
+        'crm.schemas.companies': ['Read', 'Write'],
+        'crm.schemas.contacts': ['Read', 'Write'],
+        'crm.schemas.deals': ['Read', 'Write'],
+        'crm.schemas.line_items': ['Read', 'Write'],
+        'crm.schemas.quotes': ['Read'],
       },
     },
     {
@@ -26,14 +33,22 @@ function ScopesPicker() {
       category: 'Other Common Scopes',
       scopes: {
         // Add other common scopes here, for now it's an example
-        'other.scope.example': ['Use'],
+        timeline: ['Use'],
+        tickets: ['Use'],
       },
     },
   ];
 
-  const handleScopeChange = (scope, permission, isChecked) => {
-    const fullScope = `${scope}.${permission.toLowerCase()}`;
-    if (isChecked) {
+  const handleScopeChange = (scope, permissionOrChecked, isChecked = null) => {
+    let fullScope = scope;
+    if (isChecked !== null) {
+      const permission = permissionOrChecked.toLowerCase();
+      if (permission) {
+        // check if permission is not an empty string
+        fullScope += `.${permission}`;
+      }
+    }
+    if (isChecked === null ? permissionOrChecked : isChecked) {
       setSelectedScopes((prev) => [...prev, fullScope]);
     } else {
       setSelectedScopes((prev) => prev.filter((s) => s !== fullScope));
@@ -41,51 +56,59 @@ function ScopesPicker() {
   };
 
   return (
-    <div className="scopes-picker">
-      <textarea readOnly value={selectedScopes.join(',')} rows={10} cols={50} />
-
+    <div>
+      <textarea className="scopes-output" readOnly value={selectedScopes.join(',')} rows={10} cols={50} />
       {scopesData.map((category) => (
-        <div key={category.category} className="category-section">
-          <h4>{category.category}</h4>
-          <table>
-            <thead>
-              <tr>
-                <th>Scope</th>
-                <th>Read</th>
-                <th>Write</th>
-                <th>Use</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(category.scopes).map(([scope, permissions]) => (
-                <tr key={scope}>
-                  <td>{scope}</td>
-                  <td>
-                    <input
-                      type="checkbox"
-                      disabled={!permissions.includes('Read')}
-                      onChange={(e) => handleScopeChange(scope, 'Read', e.target.checked)}
-                    />
-                  </td>
-                  <td>
-                    <input
-                      type="checkbox"
-                      disabled={!permissions.includes('Write')}
-                      onChange={(e) => handleScopeChange(scope, 'Write', e.target.checked)}
-                    />
-                  </td>
-                  <td>
-                    <input
-                      type="checkbox"
-                      disabled={!permissions.includes('Use')}
-                      onChange={(e) => handleScopeChange(scope, 'Use', e.target.checked)}
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <table key={category.category}>
+          <thead>
+            <tr>
+              <th>Scope</th>
+              {['forms', 'timeline', 'tickets'].includes(Object.keys(category.scopes)[0]) ? (
+                <th colSpan={2}></th>
+              ) : (
+                <>
+                  <th>Read</th>
+                  <th>Write</th>
+                </>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            {Object.entries(category.scopes).map(([scope, permissions]) => {
+              const isSingleCheckboxScope = ['forms', 'timeline', 'tickets'].includes(scope);
+              if (isSingleCheckboxScope) {
+                return (
+                  <tr key={scope}>
+                    <td>{scope}</td>
+                    <td colSpan={2}>
+                      <input type="checkbox" onChange={(e) => handleScopeChange(scope, '', e.target.checked)} />
+                    </td>
+                  </tr>
+                );
+              } else {
+                return (
+                  <tr key={scope}>
+                    <td>{scope}</td>
+                    <td>
+                      <input
+                        type="checkbox"
+                        disabled={!permissions.includes('Read')}
+                        onChange={(e) => handleScopeChange(scope, 'Read', e.target.checked)}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="checkbox"
+                        disabled={!permissions.includes('Write')}
+                        onChange={(e) => handleScopeChange(scope, 'Write', e.target.checked)}
+                      />
+                    </td>
+                  </tr>
+                );
+              }
+            })}
+          </tbody>
+        </table>
       ))}
     </div>
   );
