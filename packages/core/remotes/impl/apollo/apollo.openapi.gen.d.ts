a01093e18dd5cb095d301b42b82e4be683a79ccc
Tony Xiao <tonyx.ca@gmail.com>
2023-11-17 00:27:19 -0800

    Revert "chore: experiment with using @lilyrose2798/trpc-openapi to generate openapi spec"
    This reverts commit a3b60d7a02d53743bd8edb51a5b12c073edfe870.

--- packages/core/remotes/impl/apollo/apollo.openapi.gen.d.ts
+++ packages/core/remotes/impl/apollo/apollo.openapi.gen.d.ts
@@ -8,6 +8,24 @@ export interface paths {
   "/v1/emailer_campaigns/{id}": {
     get: operations["getEmailerCampaign"];
   };
+  "/v1/emailer_campaigns": {
+    post: operations["createEmailerCampaign"];
+  };
+  "/v1/emailer_campaigns/{id}/add_contact_ids": {
+    post: operations["addContactIdsToEmailerCampaign"];
+  };
+  "/v1/emailer_steps": {
+    post: operations["createEmailerStep"];
+  };
+  "/v1/emailer_steps/{id}": {
+    delete: operations["deleteEmailerStep"];
+  };
+  "/v1/emailer_touches/{id}": {
+    put: operations["updateEmailerTouch"];
+  };
+  "/v1/contacts/{id}": {
+    get: operations["getContact"];
+  };
 }
 
 export type webhooks = Record<string, never>;
@@ -42,7 +60,7 @@ export interface components {
       opt_out_rate?: components["schemas"]["metric"];
       demo_rate?: components["schemas"]["metric"];
     };
-    metric: (number | "loading") | null;
+    metric: number | "loading" | null;
     emailer_step: {
       id: string;
       emailer_campaign_id: string;
@@ -76,7 +94,7 @@ export interface components {
       emailer_template?: components["schemas"]["emailer_template"] | null;
       status?: string | null;
       /** @enum {string|null} */
-      type?: "reply_to_thread" | "new_thread" | null;
+      type?: "reply_to_thread" | "new_thread";
       include_signature?: boolean | null;
       has_personalized_opener?: boolean | null;
       personalized_opener_fallback_option?: string | null;
@@ -116,6 +134,15 @@ export interface components {
       label_ids?: string[] | null;
       prompt_id?: string | null;
     };
+    contact: {
+      id: string;
+      emailer_campaign_ids?: string[];
+      contact_campaign_statuses: {
+          id: string;
+          send_email_from_email_account_id: string;
+          emailer_campaign_id: string;
+        }[];
+    };
   };
   responses: never;
   parameters: never;
@@ -137,7 +164,6 @@ export interface operations {
       };
     };
     responses: {
-      /** @description Successful response */
       200: {
         content: {
           "application/json": {
@@ -148,75 +174,144 @@ export interface operations {
           };
         };
       };
-      /** @description Invalid input data */
-      400: {
+    };
+  };
+  createEmailerCampaign: {
+    requestBody?: {
+      content: {
+        "application/json": {
+          name?: string | null;
+          /** @enum {string} */
+          permissions?: "team_can_use" | "team_can_view" | "private";
+          user_id?: string | null;
+          label_ids?: string[];
+          active?: boolean;
+        };
+      };
+    };
+    responses: {
+      200: {
         content: {
           "application/json": {
-            /**
-             * @description The error message
-             * @example Invalid input data
-             */
-            message: string;
-            /**
-             * @description The error code
-             * @example BAD_REQUEST
-             */
-            code: string;
-            /**
-             * @description An array of issues that were responsible for the error
-             * @example []
-             */
-            issues?: {
-                message: string;
-              }[];
+            emailer_campaign: components["schemas"]["emailer_campaign"];
+            emailer_steps?: components["schemas"]["emailer_step"][] | null;
+            emailer_touches?: components["schemas"]["emailer_touch"][] | null;
+            emailer_templates?: components["schemas"]["emailer_template"][] | null;
           };
         };
       };
-      /** @description Not found */
-      404: {
+    };
+  };
+  addContactIdsToEmailerCampaign: {
+    parameters: {
+      path: {
+        id: string;
+      };
+    };
+    requestBody?: {
+      content: {
+        "application/json": {
+          contact_ids: string[];
+          emailer_campaign_id: string;
+          send_email_from_email_account_id?: string | null;
+          userId?: string | null;
+        };
+      };
+    };
+    responses: {
+      200: {
         content: {
           "application/json": {
-            /**
-             * @description The error message
-             * @example Not found
-             */
-            message: string;
-            /**
-             * @description The error code
-             * @example NOT_FOUND
-             */
-            code: string;
-            /**
-             * @description An array of issues that were responsible for the error
-             * @example []
-             */
-            issues?: {
-                message: string;
-              }[];
+            contacts: components["schemas"]["contact"][];
           };
         };
       };
-      /** @description Internal server error */
-      500: {
+    };
+  };
+  createEmailerStep: {
+    requestBody?: {
+      content: {
+        "application/json": {
+          emailer_campaign_id: string;
+          priority?: string | null;
+          position?: number | null;
+          type: components["schemas"]["emailer_step_type"];
+          wait_mode: components["schemas"]["emailer_step_wait_mode"];
+          wait_time?: number | null;
+          exact_datetime?: string | null;
+          note?: string | null;
+        };
+      };
+    };
+    responses: {
+      200: {
         content: {
           "application/json": {
-            /**
-             * @description The error message
-             * @example Internal server error
-             */
-            message: string;
-            /**
-             * @description The error code
-             * @example INTERNAL_SERVER_ERROR
-             */
-            code: string;
-            /**
-             * @description An array of issues that were responsible for the error
-             * @example []
-             */
-            issues?: {
-                message: string;
-              }[];
+            emailer_step: components["schemas"]["emailer_step"];
+            emailer_touch?: components["schemas"]["emailer_touch"] | null;
+            emailer_template?: components["schemas"]["emailer_template"] | null;
+          };
+        };
+      };
+    };
+  };
+  deleteEmailerStep: {
+    parameters: {
+      path: {
+        id: string;
+      };
+    };
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            emailer_step: {
+              id: string;
+              deleted: boolean;
+            };
+          };
+        };
+      };
+    };
+  };
+  updateEmailerTouch: {
+    parameters: {
+      path: {
+        id: string;
+      };
+    };
+    requestBody?: {
+      content: {
+        "application/json": {
+          id: string;
+          emailer_step_id?: string | null;
+          emailer_template?: components["schemas"]["emailer_template"] | null;
+          /** @enum {string|null} */
+          type?: "reply_to_thread" | "new_thread";
+        };
+      };
+    };
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            emailer_touch: components["schemas"]["emailer_touch"];
+          };
+        };
+      };
+    };
+  };
+  getContact: {
+    parameters: {
+      path: {
+        id: string;
+      };
+    };
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            contact: components["schemas"]["contact"];
           };
         };
       };
