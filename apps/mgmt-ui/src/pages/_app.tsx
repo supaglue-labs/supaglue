6299b4013e0c40493195a5dd6025e4f0c1a640a6
thomas chen <tomkit@gmail.com>
2023-11-21 14:39:46 -0800

    Update apps/mgmt-ui/src/pages/_app.tsx
    Co-authored-by: Lucas Marshall <lucasmarshall@users.noreply.github.com>

--- apps/mgmt-ui/src/pages/_app.tsx
+++ apps/mgmt-ui/src/pages/_app.tsx
@@ -348,7 +348,7 @@ function Banner(props: { createdAt: string }) {
       </div>
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-sm leading-6 text-gray-900">
-          <strong className="font-semibold">Free 30 trial</strong>
+          <strong className="font-semibold">Free 30 day trial</strong>
           <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
             <circle cx={1} cy={1} r={1} />
           </svg>
