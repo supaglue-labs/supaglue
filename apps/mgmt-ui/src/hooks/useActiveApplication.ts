1ad30032a3303a3523938f360e0192116c40162b
Lucas Marshall <lucas.marshall@gmail.com>
2023-11-21 16:59:13 -0800

    fix: banner logic for no logged in user

--- apps/mgmt-ui/src/hooks/useActiveApplication.ts
+++ apps/mgmt-ui/src/hooks/useActiveApplication.ts
@@ -5,6 +5,17 @@ import { useActiveApplicationId } from './useActiveApplicationId';
 
 export function useActiveApplication() {
   const applicationId = useActiveApplicationId();
+  if (!applicationId) {
+    return {
+      activeApplication: undefined,
+      isLoading: false,
+      error: undefined,
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      mutate: () => {},
+      isValidating: false,
+    };
+  }
+
   const { data, error, isLoading, ...rest } = useSWR(
     `/api/internal/applications/${applicationId}`,
     fetcher<Application>
