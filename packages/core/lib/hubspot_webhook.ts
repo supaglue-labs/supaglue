2fcca52f4c0a1b659c4ed4ccff070379a271b553
Ryan Liu <ryan@supaglue.com>
2023-12-11 10:54:30 -0800

    Update packages/core/lib/hubspot_webhook.ts
    Co-authored-by: Lucas Marshall <lucasmarshall@users.noreply.github.com>

--- packages/core/lib/hubspot_webhook.ts
+++ packages/core/lib/hubspot_webhook.ts
@@ -58,7 +58,7 @@ const checkWebhookTargetExists = async (hubspotClient: HubspotClient, hubspotApp
     throw e;
   }
   throw new BadRequestError(
-    `Your Hubspot Developer App already has an existing Webhook target URL. Please delete it first.`
+    `Your Hubspot Developer App already has an existing Webhook target URL. Please delete it first or use a different Developer App.`
   );
 };
 
